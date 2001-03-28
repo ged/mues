454004a0fd6cd8ce44425bda03ed6ef51243a285
Michael Granger <ged@FaerieMUD.org>
2001-03-28 21:21:44 +0000

    - Modified require to reflect name change of the Namespace module
    - Added some preliminary methods
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%4014

--- lib/mues/classlibrary.rb
+++ lib/mues/classlibrary.rb
@@ -10,11 +10,14 @@ ClassLibrary - World object class library service
 
 == Synopsis
 
-  
+  require "mues/ClassLibrary"
+  require "metaclass/Classes"
 
+  cl = MUES::ClassLibrary.new( "MyLibrary" )
+  
 == Description
 
-
+A metaclass collection container object class.
 
 == Author
 
@@ -29,7 +32,7 @@ http://language.perl.com/misc/Artistic.html)
 =end
 ###########################################################################
 
-require "mues/MUES"
+require "mues/Namespace"
 require "mues/Events"
 require "mues/Exceptions"
 
@@ -39,29 +42,56 @@ module MUES
 	class ClassError < Exception; end
 	class ClassLibrary < Object
 
-		Version = %q$Revision: 1.1 $
-		Rcsid = %q$Id: classlibrary.rb,v 1.1 2001/03/15 02:22:16 deveiant Exp $
+		Version = %q$Revision: 1.2 $
+		Rcsid = %q$Id: classlibrary.rb,v 1.2 2001/03/28 21:21:44 deveiant Exp $
 
 		attr_reader :name
 
+		### METHOD: initialize( libraryName )
+		### Initializes a class library object, setting the name of the library
+		### to the one given.
 		def initialize( libraryName )
 			super
 			@name = libraryName
 			@classes = {}
+			@interfaces = {}
+			@namespaces = {}
 		end
 
-		def addClass( klass, klassName = nil )
-			if klassName.nil?
-				klassName = klass.name
-			end
+		### METHOD: addInterface( interface[, altInterfaceName] )
+		### Adds an interface to the library, either by the name specified or
+		### the same name as the name attribute of the interface object if no
+		### name is specified.
+		def addInterface( interface, altInterfaceName = nil )
+			interfaceName = if altInterfaceName.nil?
+							then interface.name
+							else altInterfaceName
+							end
+
+			@interfaces[ interfaceName ] = klass
+		end
+
+		### METHOD: addClass( classObject[, altClassName] )
+		### Adds a class to the library, either by the name specified or the
+		### same name as the name attribute of the class object if no name is
+		### specified.
+		def addClass( klass, altClassName = nil )
+			klassName = if altClassName.nil?
+						then klass.name
+						else altClassName
+						end
 
 			@classes[ klassName ] = klass
 		end
 
+		### METHOD: getClassAncestry( className )
+		### Returns the ancestors of the class specified as an Array.
 		def getClassAncestry( className )
 			return []
 		end
 
+		### METHOD: getClassDefinition( className )
+		### Returns the eval-able definition of the class specified.
 		def getClassDefinition( className )
 			return ""
 		end
