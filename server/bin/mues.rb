d8946da2f6c480e56bac5d134c977c622b414295
Michael Granger <ged@FaerieMUD.org>
2001-04-06 08:19:19 +0000

    - Added real RD docs
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%4037

--- server/bin/mues.rb
+++ server/bin/mues.rb
@@ -1,21 +1,42 @@
 #!/usr/bin/ruby
-#
-#	Monolithic Server Thingie
-#
-#
-#
-#
+###########################################################################
+=begin
 
-require "mues/Config"
+=Name
+
+mues.rb - Server startup script
+
+=Synopsis
+
+  $ mues.rb
+
+=Description
+
+A basic non-forking server startup script.
+
+=Author
+
+ Michael Granger <((<ged@FaerieMUD.org|URL:mailto:ged@FaerieMUD.org>))>
+
+ Copyright (c) 2000-2001 The FaerieMUD Consortium. All rights reserved.
+
+ This module is free software. You may use, modify, and/or redistribute this
+ software under the terms of the Perl Artistic License. (See
+ http://language.perl.com/misc/Artistic.html)
+
+=end
+###########################################################################
+
 require "mues/Engine"
+require "mues/Config"
 
-$ConfigFile = "MUES.cfg"
+ConfigFile = "MUES.cfg"
 
 ### Main function
 def main
 
 	### Instantiate the configuration and the server objects
-	config = MUES::Config.new( $ConfigFile )
+	config = MUES::Config.new( ConfigFile )
 	engine = MUES::Engine.instance
 
 	### Start up and run the server
