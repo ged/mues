e872a122e519a976df68f7d68b7b4fce50bdca41
Michael Granger <ged@FaerieMUD.org>
2001-05-14 12:35:59 +0000

    - Renamed a bunch of fields for clarity.
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%4081

--- sql/mues.user.sql
+++ sql/mues.user.sql
@@ -1,7 +1,8 @@
 /*
+
    MUES Player Table Schema (MySQL)
-	$Id: mues.user.sql,v 1.1 2001/03/15 02:22:16 deveiant Exp $
-	Time-stamp: <28-Jan-2001 02:38:09 deveiant>
+	$Id: mues.user.sql,v 1.2 2001/05/14 12:35:59 deveiant Exp $
+	Time-stamp: <25-Apr-2001 12:22:35 deveiant>
 
 	Michael Granger <ged@FaerieMUD.org>
 	Copyright (c) 1998-2001 The FaerieMUD Consortium. All rights reserved.
@@ -21,25 +22,21 @@ CREATE TABLE player (
 	ts					TIMESTAMP(12),
 	
 	/* Data fields */
+	playerVersion		VARCHAR(10)		NOT NULL,
 	username			VARCHAR(50) 	NOT NULL,
-	cryptedPass			VARCHAR(20)		NOT NULL,
+	cryptedPass			VARCHAR(20)		NOT NULL,							-- MD5 hexdigest
 	realname			VARCHAR(75),
 	emailAddress		VARCHAR(75),
 	lastLogin			DATETIME,
 	lastHost			VARCHAR(75),
 
-	dateCreated			DATETIME,
-	age					INT				UNSIGNED NOT NULL DEFAULT 0,
+	timeCreated			DATETIME,
+	firstLoginTick		INT				UNSIGNED NOT NULL DEFAULT 0,		-- Tick of first login
 
-	level				ENUM(
-							"player",
-							"creator",
-							"implementor",
-							"admin"
-							)			NOT NULL DEFAULT 'player',
-
-	preferences			BLOB,
-	characters			BLOB,
+	role				TINYINT			UNSIGNED NOT NULL DEFAULT 0,		-- Permissions role
+	flags				INT				UNSIGNED NOT NULL DEFAULT 0,		-- Bitfield
+	preferences			BLOB,												-- Frozen Ruby hash
+	characters			BLOB,												-- Frozen Ruby hash
 
 	/* Indexes */
 	UNIQUE( username )
