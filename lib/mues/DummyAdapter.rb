baeedaf23fececcd507637a9abdfd353f3445b98
Michael Granger <ged@FaerieMUD.org>
2001-05-14 12:20:10 +0000

    - Updated to match the 1.4 interface.
    - Added storePlayerData(), fetchPlayerData(), and createPlayerData() methods.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%4069

--- lib/mues/DummyAdapter.rb
+++ lib/mues/DummyAdapter.rb
@@ -43,12 +43,13 @@ module MUES
 
 			include Debuggable
 
-			Version = %q$Revision: 1.2 $
-			Rcsid = %q$Id: DummyAdapter.rb,v 1.2 2001/03/29 02:47:05 deveiant Exp $
+			Version = /([\d\.]+)/.match( %q$Revision: 1.3 $ )[1]
+			Rcsid = %q$Id: DummyAdapter.rb,v 1.3 2001/05/14 12:20:10 deveiant Exp $
 
 			attr_accessor :db, :host, :user, :password
 
-			### METHOD: initialize( db, host, user, password )
+			### METHOD: new( db, host, user, password )
+			### Create a new DummyAdapter ObjectStore adapter object.
 			def initialize( db, host, user, password )
 				@db = db
 				@host = host
@@ -58,64 +59,134 @@ module MUES
 				@dbDir = @db.gsub( /[^a-zA-Z0-9]+/, "" )
 				unless FileTest.directory?( @dbDir )
 					Dir.mkdir( @dbDir, 0755 )
+					Dir.mkdir( "#{@dbDir}/players", 0755 )
 				end
 			end
 
-			### METHOD: storeObject( obj )
-			def storeObject( obj )
-				raise TypeError, "Cannot store a '#{obj.class.name}' object" unless
-					obj.is_a?( MUES::Object )
+			### METHOD: storeObjects( *objects )
+			### Store the specified objects in the objectstore.
+			def storeObjects( *objects )
+				oids = []
+				objects.each {|obj|
+					# Make an object id out of the class name and the MUES id
+					oid = _safeifyId( "%s:%s" % [ obj.class.name, obj.muesid ] )
+					oids << oid
 
-				# Make an object id out of the class name and the MUES id
-				oid = _safeifyId( "%s:%s" % [ obj.class.name, obj.muesid ] )
-
-				# Open a file with the oid as the name and dump the object to it
-				File.open( "#{@dbDir}/#{oid}", File::CREAT|File::TRUNC|File::RDWR, 0644 ) { |f|
-					until f.flock( File::LOCK_EX|File::LOCK_NB )
-						sleep 0.2
-					end
-					Marshal.dump( obj, f )
-					f.flock( File::LOCK_UN )
-				}
-
-				return oid
-			end
-
-			### METHOD: fetchObject( id )
-			def fetchObject( id )
-				obj = nil
-
-				# Make the oid into a safe filename
-				oid = _safeifyId( id )
-
-				# Try to open the corresponding file, returning nil if we fail
-				# After opening and locking, delete the file before reading.
-				begin
-					File.open( "#{@dbDir}/#{oid}", File::RDONLY ) { |f|
+					# Open a file with the oid as the name and dump the object to it
+					File.open( "#{@dbDir}/#{oid}", File::CREAT|File::TRUNC|File::RDWR, 0644 ) { |f|
 						until f.flock( File::LOCK_EX|File::LOCK_NB )
 							sleep 0.2
 						end
-						obj = Marshal.restore( f )
-						File.delete( oid )
+						Marshal.dump( obj, f )
 						f.flock( File::LOCK_UN )
 					}
-				rescue IOError => e
-					raise NoSuchObjectError, e.message
-				end
+				}
 
-				return obj
+				return oids
 			end
 
+			### METHOD: fetchObjects( *oids )
+			### Fetch the objects with the specified oids.
+			def fetchObjects( *ids )
+				objs = []
+
+				ids.each {|id|
+					# Make the oid into a safe filename
+					oid = _safeifyId( id )
+
+					# Try to open the corresponding file, returning nil if we fail
+					# After opening and locking, delete the file before reading.
+					begin
+						File.open( "#{@dbDir}/#{oid}", File::RDONLY ) { |f|
+							until f.flock( File::LOCK_EX|File::LOCK_NB )
+								sleep 0.2
+							end
+							obj = Marshal.restore( f )
+							f.flock( File::LOCK_UN )
+						}
+					rescue IOError => e
+						raise NoSuchObjectError, e.message
+					end
+
+					objs << obj
+				}
+
+				return objs
+			end
+
+
 			### METHOD: stored?( id )
+			### Returns true if the id specified corresponds to a stored object.
 			def stored?( id )
 				oid = _safeifyId( id )
 				return FileTest.exists?( "#{@dbDir}/#{oid}" )
 			end
 
+
+			### METHOD: storePlayerData( username, data )
+			### Store the given data as the player record for the specified username
+			def storePlayerData( username, data )
+				filename = _safeifyId( username )
+				
+				# Open a file with the playername as the name and dump the object to it
+				File.open( "#{@dbDir}/players/#{filename}", File::CREAT|File::TRUNC|File::RDWR, 0644 ) { |f|
+					until f.flock( File::LOCK_EX|File::LOCK_NB )
+						sleep 0.2
+					end
+					Marshal.dump( data, f )
+					f.flock( File::LOCK_UN )
+				}
+			end
+
+			### METHOD: fetchPlayerData( username )
+			### Fetch the player record for the given username. Throws a
+			### NoSuchObjectError exception if the player record does not exist.
+			def fetchPlayerData( username )
+				filename = _safeifyId( username )
+
+				# Try to open the corresponding file, returning nil if we fail
+				# After opening and locking, delete the file before reading.
+				begin
+					File.open( "#{@dbDir}/players/#{filename}", File::RDONLY ) { |f|
+						until f.flock( File::LOCK_EX|File::LOCK_NB )
+							sleep 0.2
+						end
+						obj = Marshal.restore( f )
+						f.flock( File::LOCK_UN )
+					}
+				rescue IOError => e
+					raise NoSuchObjectError, e.message
+				end
+			end
+
+			### METHOD: createPlayerData( username )
+			### Create a player record for the given username and return it
+			def createPlayerData( username )
+				filename = _safeifyId( username )
+				raise AdapterError, "A player with the name '#{username}' already exists" if
+					FileTest.exists?( "#{@dbDir}/players/#{filename}" )
+
+				data = MUES::Player::DefaultDbInfo.dup
+				storePlayerData( username, data )
+
+				return data
+			end
+
+			### METHOD: deletePlayerData( username )
+			### Delete the player data associated with the given username
+			def deletePlayerData( username )
+				filename = _safeifyId( username )
+				raise AdapterError, "No player with the name '#{username}' exists" unless
+					FileTest.exists?( "#{@dbDir}/players/#{filename}" )
+
+				File.delete( "#{@dbDir}/players/#{filename}" )
+			end
+				
 			protected
 
 			### METHOD: safeifyId( id )
 			def _safeifyId( id )
+				checkType( id, String )
 				return id.gsub( /[^:a-zA-Z0-9]+/, "" )
 			end
 
