daf6f98df5ea01e6a74c3b1f11c54b4c8c3d16ce
Michael Granger <ged@FaerieMUD.org>
2001-05-14 12:18:55 +0000

    - Changed all Mutexen to Syncs to support shared locking.
    - Added storePlayerData(), fetchPlayerData(), and createPlayerData() methods.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%4068

--- lib/mues/BdbAdapter.rb
+++ lib/mues/BdbAdapter.rb
@@ -35,6 +35,7 @@ http://language.perl.com/misc/Artistic.html)
 require "bdb"
 require "thread"
 require "ftools"
+require "sync"
 
 require "mues/Namespace"
 require "mues/Exceptions"
@@ -48,28 +49,38 @@ module MUES
 
 			include Debuggable
 
-			Version = %q$Revision: 1.3 $
-			Rcsid = %q$Id: BdbAdapter.rb,v 1.3 2001/03/29 02:47:05 deveiant Exp $
-
+			### Class constants
+			Version = /([\d\.]+)/.match( %q$Revision: 1.4 $ )[1]
+			Rcsid = %q$Id: BdbAdapter.rb,v 1.4 2001/05/14 12:18:55 deveiant Exp $
 			DirectoryName = 'objectstore-bdb'
 
-			### METHOD: initialize( db, host, user, password )
-			### Initialize the adapter. Only the 'db' argument is used.
+			### Class variables
+			@@Sections = %w{object player ban allow}
+
+			### METHOD: new( db, host, user, password )
+			### Creates a new BerkeleyDB ObjectStore adapter. Only the 'db'
+			### argument is used.
 			def initialize( db, *ignored )
+
+				# If the BDB objectstore directory doesn't yet exist, make it
 				unless File.directory?( DirectoryName )
 					Dir.mkdir( DirectoryName )
 				end
 
-				@env = BDB::Env.new( DirectoryName, BDB::CREATE|BDB::INIT_TRANSACTION )
-
-				@db = db
-				@dbh = @env.open_db( BDB::HASH, 'ObjectStore', @db, BDB::CREATE )
-				@lock = Mutex.new
+				# Initialize attributes
+				@env	= BDB::Env.new( DirectoryName, BDB::CREATE|BDB::INIT_TRANSACTION )
+				@db		= db
+				@dbh	= {}
+				@@Sections.each {|key| @dbh[key] = @env.open_db( BDB::HASH, @db, key, BDB::CREATE )}
+				@lock	= {}
+				@@Sections.each {|key| @lock[key] = Sync.new}
 
 				true
 			end
 
+
 			### METHOD: storeObject( *objects )
+			### Store the specified objects in the database
 			def storeObjects( *objects )
 				oids = []
 
@@ -87,8 +98,8 @@ module MUES
 
 					### Dump the object and store it
 					rawObj = Marshal.dump(obj)
-					@lock.synchronize {
-						@dbh.store( oid, rawObj )
+					@lock['object'].synchronize(Sync::EX) {
+						@dbh['object'].store( oid, rawObj )
 					}
 
 					oids.push oid
@@ -97,16 +108,17 @@ module MUES
 				return oids
 			end
 
+
 			### METHOD: fetchObjects( *oids )
+			### Fetch objects with the specified ids from the database and return them
 			def fetchObjects( *oids )
 				objects = []
 
 				### Iterate over each id, re-instantiating each corresponding
 				### object
 				oids.each {|oid|
-					rawObj = nil
-					@lock.synchronize {
-						rawObj = @dbh.fetch( oid, BDB::RMW )
+					@lock['object'].synchronize(Sync::SH) {
+						rawObj = @dbh['object'].fetch( oid )
 					}
 
 					obj = Marshal.restore(rawObj)
@@ -114,6 +126,74 @@ module MUES
 				}
 
 				return objects
+			end
+
+
+			### METHOD: hasObject?( id )
+			### Returns true if an entry with the specified id exists in the database
+			def hasObject?( id )
+				@lock['object'].synchronize(Sync::SH) {
+					return @dbh['object'].has_key?( id )
+				}
+			end
+
+
+			### METHOD: storePlayerData( username, playerDataHash )
+			### Store the specified hash of player data for the specified user
+			def storePlayerData( username, data )
+				checkType( username, String )
+				checkType( data, Hash )
+
+				data['username'] = username
+				frozenData = Marshal.dump( data )
+				@lock['player'].synchronize(Sync::EX) {
+					@dbh['player'].store( username, frozenData )
+				}
+
+				return data
+			end
+
+
+			### METHOD: fetchPlayerData( username )
+			### Fetch the hash of player data for the specified user
+			def fetchPlayerData( username )
+				checkType( username, String )
+
+				frozenData = @lock['player'].synchronize(Sync::SH) {
+					@dbh['player'].fetch( username )
+				}
+				return nil if frozenData.nil?
+				return Marshal.restore( frozenData )
+			end
+
+
+			### METHOD: createPlayerData( username )
+			### Create a new hash of player data with the specified username
+			def createPlayerData( username )
+				checkType( username, String )
+
+				@lock['player'].synchronize(Sync::SH) {
+					raise AdapterError, "A player with the name '#{username}' already exists." if
+						@dbh['player'].has_key?( username )
+
+					storePlayerData( username, MUES::Player::DefaultDbInfo.dup )
+				}
+			end
+
+
+			### METHOD: deletePlayerData( username )
+			### Delete the hash of player data for the specified username
+			def deletePlayerData( username )
+				checkType( username, String )
+
+				@lock['player'].synchronize(Sync::SH) {
+					raise AdapterError, "No player with the name '#{username}' exists." unless
+						@dbh['player'].has_key?( username )
+
+					@lock['player'].synchronize(Sync::EX) {
+						@dbh['player'].delete( username )
+					}
+				}
 			end
 
 		end # Class BdbAdapter
