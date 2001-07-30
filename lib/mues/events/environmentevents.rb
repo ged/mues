06a1f7df2398bdb235586feba75099575d956e7a
Michael Granger <ged@FaerieMUD.org>
2001-07-30 12:12:51 +0000

    - Renamed from WorldEvents.rb
    - Added EnvironmentEvent, LoadEnvironmentEvent, and UnloadEnvironmentEvent
      classes
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40163

--- lib/mues/events/environmentevents.rb
+++ lib/mues/events/environmentevents.rb
@@ -1,20 +1,22 @@
 #!/usr/bin/ruby
-###########################################################################
+#################################################################
 =begin
 
-=WorldEvents.rb
+=EnvironmentEvents.rb
 
 == Name
 
-WorldEvents - A collection of world event classes
+EnvironmentEvents - A collection of environment event classes
 
 == Synopsis
 
-  
+  LoadEnvironmentEvent.new( environmentNameString )
+  UnloadEnvironmentEvent.new( environmentNameString )  
 
 == Description
 
-
+This module contains event classes the instances of which are used to interact
+with Environment objects in the MUES.
 
 == Author
 
@@ -27,7 +29,7 @@ software under the terms of the Perl Artistic License. (See
 http://language.perl.com/misc/Artistic.html)
 
 =end
-###########################################################################
+#################################################################
 
 require "mues/Namespace"
 require "mues/Exceptions"
@@ -36,14 +38,64 @@ require "mues/events/BaseClass"
 
 module MUES
 
-	###########################################################################
+	#################################################################
 	###	A B S T R A C T   E V E N T   C L A S S E S
-	###########################################################################
+	#################################################################
 
-	### (ABSTRACT) CLASS: WorldEvent < Event
-	class WorldEvent < Event ; implements AbstractClass
+	### (ABSTRACT) CLASS: EnvironmentEvent < Event
+	class EnvironmentEvent < Event ; implements AbstractClass, Debuggable
+
+		attr_reader :user
+
+		def initialize( user=nil )
+			checkType( user, NilClass, MUES::User )
+			@user = user
+			super()
+		end
 	end
 
+
+	#################################################################
+	###	C O N C R E T E   E V E N T   C L A S S E S
+	#################################################################
+
+	### CLASS: LoadEnvironmentEvent < EnvironmentEvent
+	class LoadEnvironmentEvent < EnvironmentEvent
+
+		attr_reader :name, :spec
+		
+		### METHOD: initialize( name, spec[, user=MUES::User] )
+		### Intitialize the event to load an instance of the environment spec
+		### specified and associate it with the given name. If the optional user
+		### argument is given, the status of the event will be sent to them as
+		### an OutputEvent.
+		def initialize( name, spec, user=nil )
+			checkEachType( [name,spec], String )
+			checkType( user, NilClass, MUES::User )
+
+			@name = name
+			@spec = spec
+
+			super( user )
+		end
+
+	end
+
+	### CLASS: UnloadEnvironmentEvent < EnvironmentEvent
+	class UnloadEnvironmentEvent < EnvironmentEvent
+		attr_reader :name
+
+		### METHOD: initialize( envOrEnvname[, user=MUES::User] )
+		### Initialize the object with the environment specified as its
+		### target. The target may be either an environment object or the name
+		### associated with it in the Engine.
+		def initialize( name, user=nil )
+			checkType( name, ::String )
+
+			@name = name
+			super( user )
+		end
+	end
 
 end # module MUES
 
