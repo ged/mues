64c623f5cf855c4614570b6ebedb92ddbdcaabec
Michael Granger <ged@FaerieMUD.org>
2001-11-01 17:14:13 +0000

    - Updated documentation
    - Fixed a bug in createUser() - New user objects now have their username set.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40226

--- lib/mues/ObjectStore.rb
+++ lib/mues/ObjectStore.rb
@@ -11,7 +11,8 @@ ObjectStore - An object persistance abstraction class
 == Synopsis
 
   require "mues/ObjectStore"
-  oStore = ObjectStore.new( "MySQL", "faeriemud", "localhost", "fmuser", "fmpass" )
+  require "mues/Config"
+  oStore = MUES::ObjectStore.new( MUES::Config.new("MUES.cfg") )
 
   objectIds = oStore.storeObjects( obj ) {|obj|
 	$stderr.puts "Stored object #{obj}"
@@ -25,6 +26,94 @@ This class is a generic front end to various means of storing MUES objects. It
 uses one or more configurable back ends which serialize and store objects to
 some kind of storage medium (flat file, database, sub-atomic particle inference
 engine), and then later can restore and de-serialize them.
+
+== Classes
+=== MUES::ObjectStore
+==== Class Methods
+
+--- MUES::ObjectStore.hasAdapter?( name )
+
+    Returns true if the object store has an adapter class named ((|name|)).
+
+--- MUES::ObjectStore.getAdapter( driver, db, host, user, password )
+
+    Get a new back-end adapter object for the specified ((|driver|)), ((|db|)),
+    ((|host|)), ((|user|)), and ((|password|)).
+
+==== Protected Class Methods
+
+--- MUES::ObjectStore._getAdapterClass( name )
+
+    Returns the adapter class associated with the specified ((|name|)), or
+    (({nil})) if the class is not registered with the ObjectStore.
+
+--- MUES::ObjectStore._loadAdapters
+
+    Search for adapters in the subdir specified in the (({AdapterSubdir})) class
+    constant, attempting to load each one.
+
+==== Public Methods
+
+--- MUES::ObjectStore#createUser( username ) { |obj| block } -> User
+
+    Returns a new MUES::User object for the given ((|username|)). If the
+    optional ((|block|)) is given, it will be passed the user object as an
+    argument. When the block exits, the user object will be automatically stored
+    and de-allocated. In this case, (({ObjectStore.fetchUser})) returns
+    (({true})). If no block is given, the new MUES::User object is returned.
+
+--- MUES::ObjectStore#deleteUser( username )
+
+    Deletes the user associated with the specified ((|username|)) from the
+    objectstore.
+
+--- MUES::ObjectStore#fetchObjects( *objectIds ) { |obj| block } -> objects=Array
+
+    Fetch the objects associated with the given ((|objectIds|)) from the
+    objectstore and call (({awaken()})) on them if they respond to such a
+    method. If the optional ((|block|)) is specified, it is used as an iterator,
+    being called with each new object in turn. If the block is specified, this
+    method returns the array of the results of each call; otherwise, the fetched
+    objects are returned.
+
+--- MUES::ObjectStore#fetchUser( username ) { |obj| block } -> User
+
+    Returns a user object for the username specified unless the optional code
+    block is given, in which case it will be passed the user object as an
+    argument. When the block exits, the user object will be automatically stored
+    and de-allocated, and (({true})) is returned if storing the user object
+    succeeded. If the user doesn^t exist, (({ObjectStore.fetchUser})) returns
+    (({nil})).
+
+--- MUES::ObjectStore#getUserList()
+
+    Returns an array of usernames that exist in the objectstore
+
+--- MUES::ObjectStore#hasObject?( id )
+
+    Return true if the ObjectStore contains an object associated with the
+    specified ((|id|)).
+
+--- MUES::ObjectStore#storeObjects( *objects ) { |oid| block }-> oids=Array
+
+    Store the given ((|objects|)) in the ObjectStore after calling (({lull()}))
+    on each of them, if they respond to such a method. If the optional
+    ((|block|)) is given, it is used as an iterator by calling it with each
+    object id after the objects are stored, and then returning the results of
+    each call in an Array. If no block is given, the object ids are returned.
+
+--- MUES::ObjectStore#storeUser( user=MUES::User ) -> true
+
+    Store the given ((|user|)) in the datastore, returning (({true})) on
+    success.
+
+==== Protected Methods
+
+--- MUES::ObjectStore#initialize( config=MUES::Config )
+
+    Initialize a new ObjectStore based on the values in the specified
+    configuration. If the specified ((|driver|)) cannot be loaded, an
+    (({UnknownAdapterError})) exception is raised.
 
 == Author
 
@@ -59,8 +148,8 @@ module MUES
 		autoload "MUES::ObjectStore::Adapter", "mues/adapters/Adapter"
 
 		### Class Constants
-		Version = /([\d\.]+)/.match( %q$Revision: 1.9 $ )[1]
-		Rcsid = %q$Id: ObjectStore.rb,v 1.9 2001/08/05 05:45:30 deveiant Exp $
+		Version = /([\d\.]+)/.match( %q$Revision: 1.10 $ )[1]
+		Rcsid = %q$Id: ObjectStore.rb,v 1.10 2001/11/01 17:14:13 deveiant Exp $
 
 		AdapterSubdir = 'mues/adapters'
 		AdapterPattern = /#{AdapterSubdir}\/(\w+Adapter).rb$/	#/
@@ -128,32 +217,31 @@ module MUES
 
 			public
 
-			### (CLASS) METHOD: _hasAdapter?( name )
+			### (CLASS) METHOD: hasAdapter?( name )
 			### Returns true if the object store has an adapter class named
 			### ((|name|)).
 			def hasAdapter?( name )
 				return _getAdapterClass( name ).is_a?( Class )
 			end
 
-			### (CLASS) METHOD: getAdapter( driver, db, host, user, password )
-			### Get a new back-end adapter object for the specified
-			### ((|driver|)), ((|db|)), ((|host|)), ((|user|)), and
-			### ((|password|)).
-			def getAdapter( driver, db, host, user, password )
+			### (CLASS) METHOD: getAdapter( config=MUES::Config )
+			### Get a new back-end adapter object for the driver specified by the ((|config|)).
+			def getAdapter( config )
 				_loadAdapters()
+				driver = config["objectstore"]["driver"]
 				klass = _getAdapterClass( driver )
 				raise UnknownAdapterError, "Could not fetch adapter class '#{driver}'" unless klass
-				klass.new( db, host, user, password )
+				klass.new( config )
 			end
 		end
 
-		### (PROTECTED) METHOD: initialize( driver="Bdb", db="mues", host, user, password )
-		### Initialize a new ObjectStore with the specified arguments. If the
-		### specified ((|driver|)) cannot be loaded, an
-		### (({UnknownAdapterError})) exception is raised.
-		def initialize( driver = "Bdb", db = "mues", host = nil, user = nil, password = nil )
+		### (PROTECTED) METHOD: initialize( config=MUES::Config )
+		### Initialize a new ObjectStore based on the values in the specified
+		### configuration object. If the specified ((|driver|)) cannot be
+		### loaded, an (({UnknownAdapterError})) exception is raised.
+		def initialize( config )
 			super()
-			@dbAdapter = self.class.getAdapter( driver, db, host, user, password )
+			@dbAdapter = self.class.getAdapter( config )
 		end
 
 		### METHOD: fetchObjects( *objectIds ) { |obj| block } -> objects=Array
@@ -229,18 +317,18 @@ module MUES
 			return true
 		end
 
-		### METHOD: createUser( username, role ) { |obj| block } -> User
-		### Returns a new MUES::User object with the permissions specified by
-		### ((|role|)) for the given ((|username|)). If the optional ((|block|))
-		### is given, it will be passed the user object as an argument. When the
-		### block exits, the user object will be automatically stored and
-		### de-allocated. In this case, (({ObjectStore.fetchUser})) returns
-		### (({true})).If no block is given, the new MUES::User object is
-		### returned.
-		def createUser( username, role=MUES::User::Role::USER )
+		### METHOD: createUser( username ) { |obj| block } -> User
+		### Returns a new MUES::User object with the given ((|username|)). If
+		### the optional ((|block|)) is given, it will be passed the user object
+		### as an argument. When the block exits, the user object will be
+		### automatically stored and de-allocated. In this case,
+		### (({ObjectStore.fetchUser})) returns (({true})).If no block is given,
+		### the new MUES::User object is returned.
+		def createUser( username )
 			userData = @dbAdapter.createUserData( username )
 
 			user = User.new( userData )
+			user.username = username
 
 			if block_given?
 				yield( user )
