d9d0f1dd00faaead7d7cf0b5cd0c6e42950ae9ee
Michael Granger <ged@FaerieMUD.org>
2001-11-01 17:52:07 +0000

    - Updated documentation
    - Fixed select loop bug. Writable handle now is only select()ed when there's
      something in the write buffer.
    
    - Added SelectTimeout class variable
    
    - Added write(), _sendShutdownMessage() methods
    
    - Abstracted the input handling mechanism from the IO loop to facilitate
      subclasses handling their own input without needing to re-implement the IO
      loop.
    
    - Separated maintenance of the input buffer from parsing of it, again to allow
      subclasses to override either one separately.
    
    - Added a windowSize attribute and accessor
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40236

--- lib/mues/filters/socketoutputfilter.rb
+++ lib/mues/filters/socketoutputfilter.rb
@@ -18,6 +18,75 @@ SocketOutputFilter - An IP socket output filter class
 Instances of this class are participants in an IOEventStream chain of
 responsibility, sending output and reading input from a TCPSocket.
 
+== Modules
+=== MUES::SocketOutputFilter::State
+
+A namespace for type constants. Contains:
+
+: State::CONNECTED
+
+  The filter contains a connected socket.
+
+: State::DISCONNECTED
+
+  The filter does not contain a connected socket.
+
+== Classes
+=== MUES::SocketOutputFilter
+
+==== Constructor
+
+--- MUES::SocketOutputFilter.new( socket )
+
+    Initialize the filter with the specified ((|socket|)).
+
+==== Public Methods
+
+--- MUES::SocketOutputFilter#queueOutputEvents( *events )
+
+    Add the data from the specified ((|events|)) to the output buffer for
+    transmission.
+
+--- MUES::SocketOutputFilter#readBuffer
+
+    Return the filter^s read buffer.
+
+--- MUES::SocketOutputFilter#writeBuffer
+
+    Return the filter^s write buffer.
+
+--- MUES::SocketOutputFilter#remoteHost
+
+    Return the value of the remoteHost attribute.
+
+--- MUES::SocketOutputFilter#puts( aString )
+
+    Append a string directly onto the output buffer. Useful when doing direct
+    output and flush.
+
+--- MUES::SocketOutputFilter#shutdown
+
+    Shut the filter down.
+
+==== Protected Methods
+
+--- MUES::SocketOutputFilter#_ioThreadRoutine( socket )
+
+    Thread routine for socket IO multiplexing. Reads data from queued output
+    events and sends it to the remote client, and creates new input events
+    from user input.
+
+--- MUES::SocketOutputFilter#_parseInputBuffer( inputBuffer )
+
+    Parse input events from the given raw buffer and return the
+    (possibly) modified buffer after queueing any input events created.
+
+--- MUES::SocketOutputFilter#_sendShutdownMessage( rawSocket )
+
+    Send a shutdown message to the client using unbuffered I/O on the
+    ((|rawSocket|)) specified, as we won^t be around to fetch it from
+    the buffer.
+
 == Author
 
 Michael Granger <((<ged@FaerieMUD.org|URL:mailto:ged@FaerieMUD.org>))>
@@ -49,8 +118,8 @@ module MUES
 		end
 
 		### Class constants
-		Version = /([\d\.]+)/.match( %q$Revision: 1.7 $ )[1]
-		Rcsid = %q$Id: socketoutputfilter.rb,v 1.7 2001/09/26 13:30:39 deveiant Exp $
+		Version = /([\d\.]+)/.match( %q$Revision: 1.8 $ )[1]
+		Rcsid = %q$Id: socketoutputfilter.rb,v 1.8 2001/11/01 17:52:07 deveiant Exp $
 		DefaultSortPosition = 300
 
 		NULL = "\000"
@@ -60,8 +129,9 @@ module MUES
 
 		### Class attributes
 		@@MTU = 4096				# Maximum Transmissable Unit
+		@@SelectTimeout = 0.1	# The number of seconds to wait in select()
 
-		### (PROTECTED) METHOD: initialize( socket )
+		### (PROTECTED) METHOD: initialize( socket [, order] )
 		### Initialize the filter
 		protected
 		def initialize( aSocket, order=DefaultSortPosition )
@@ -69,13 +139,12 @@ module MUES
 			super( order )
 
 			@readBuffer = ''
-			@readMutex = Sync.new
 			@writeBuffer = ''
 			@writeMutex = Sync.new
 			@state = State::DISCONNECTED
 			@remoteHost = aSocket.peeraddr[2]
 
-			@mode = ''
+			@windowSize = { 'height' => 23, 'width' => 80 }
 
 			@socketThread = Thread.new { _ioThreadRoutine(aSocket) }
 			@socketThread.desc = "SocketOutputFilter IO thread [fd: #{aSocket.fileno}, peer: #{@remoteHost}]"
@@ -88,10 +157,10 @@ module MUES
 		public
 
 		# Accessors
-		attr_reader :readBuffer, :writeBuffer, :remoteHost
+		attr_reader :readBuffer, :writeBuffer, :remoteHost, :windowSize
 
-		### handleOutputEvents( *events )
-		### Handle an output event by appending its data to the output buffer
+		### METHOD: handleOutputEvents( *events )
+		### Handle output events by appending their data to the output buffer
 		def handleOutputEvents( *events )
 			events = super( *events )
 			events.flatten!
@@ -113,11 +182,20 @@ module MUES
 
 
 		### METHOD: puts( aString )
-		### Append a string directly onto the output buffer. Useful when doing
-		### direct output and flush.
+		### Append a string directly onto the output buffer with a
+		### line-ending. Useful when doing direct output and flush.
 		def puts( aString )
 			@writeMutex.synchronize(Sync::EX) {
 				@writeBuffer << aString + "\n"
+			}
+		end
+
+		### METHOD: write( aString )
+		### Append a string directly onto the output buffer without a line
+		### ending. Useful when doing direct output and flush.
+		def write( aString )
+			@writeMutex.synchronize(Sync::EX) {
+				@writeBuffer << aString
 			}
 		end
 
@@ -135,9 +213,9 @@ module MUES
 		protected
 
 		### (PROTECTED) METHOD: _ioThreadRoutine( socket )
-		###	Thread routine for socket IO multiplexing. Reads data from queued output
-		###		events and sends it to the remote client, and creates new input events
-		###		from user input.
+		### Thread routine for socket IO multiplexing. Reads data from queued
+		### output events and sends it to the remote client, and creates new
+		### input events from user input.
 		def _ioThreadRoutine( socket )
 			_debugMsg( 1, "In IO thread routine." )
 			mySocket = socket
@@ -145,45 +223,14 @@ module MUES
 
 			### Multiplex I/O, catching IO exceptions
 			begin
-				readable = []
-				writeable = []
-
-				### Loop until we break or get shut down
-				loop do
-					readable, writable = select( [mySocket], [mySocket] )
-
-					### Read any input from the socket if it's ready
-					unless readable.empty?
-						@readMutex.synchronize(Sync::EX) {
-							@readBuffer += mySocket.sysread( @@MTU )
-							_debugMsg( 5, "Read data in select loop (@readBuffer = '#{@readBuffer}', " +
-									   "length = #{@readBuffer.length})." )
-
-							unless @readBuffer.empty?
-								_debugMsg( 4, "Read buffer has stuff in it. Trying to get input events from it." )
-								@readBuffer = _parseRawInput( @readBuffer )
-							end
-						}
-					end
-
-					### Write any buffered output to the socket if we have
-					### output pending and the socket is writable
-					unless writable.empty? || @writeBuffer.empty?
-						_debugMsg( 5, "Writing in select loop (@writebuffer = '#{@writeBuffer}')." )
-						@writeMutex.synchronize(Sync::EX) {
-							bytesWritten = mySocket.syswrite( @writeBuffer )
-							@writeBuffer[0 .. bytesWritten] = ''
-						}
-					end
-				end
+				_ioLoop( mySocket )
 
 			### Handle EOF on the socket by setting the state and 
 			rescue EOFError => e
 				engine.dispatchEvents( LogEvent.new("info", "SocketOutputFilter shutting down: #{e.message}") )
 
 			rescue Shutdown
-				mySocket.syswrite( @writeBuffer )
-				mySocket.syswrite( "\n>>> Disconnecting <<<\n\n" )
+				self._sendShutdownMessage( mySocket )
 
 			### Just log any other caught exceptions (for now)
 			rescue StandardError => e
@@ -203,10 +250,98 @@ module MUES
 		end
 
 		
-		### (PROTECTED) METHOD: _parseRawInput( inputBuffer )
+		### (PROTECTED) METHOD: _sendShutdownMessage( rawSocket )
+		### Send a shutdown message to the client using unbuffered I/O on the
+		### ((|rawSocket|)) specified, as we won't be around to fetch it from
+		### the buffer.
+		def _sendShutdownMessage( mySocket )
+			mySocket.syswrite( @writeBuffer )
+			mySocket.syswrite( "\n>>> Disconnecting <<<\n\n" )
+		end
+
+
+		### (PROTECTED) METHOD: _ioLoop( socket )
+		### Multiplex reading and writing from the given ((|socket|)) object, 
+		def _ioLoop( mySocket )
+			readable = [mySocket]
+			writable = []
+			errors = [mySocket]
+
+			### Loop until we break or get shut down
+			loop do
+
+				# The socket only goes into the writable array if there's
+				# something to write.
+				if @writeBuffer.empty?
+					writable -= [ mySocket ]
+				else
+					writable += [ mySocket ]
+				end
+
+				# Select on the socket
+				rsock, wsock, esock = select( readable, writable, errors, @@SelectTimeout )
+				_debugMsg( 5, "Readable: %s, writable: %s, errors: %s" % [
+							  rsock.nil? ? "(nil)" : rsock.length.to_s,
+							  wsock.nil? ? "(nil)" : wsock.length.to_s,
+							  esock.nil? ? "(nil)" : esock.length.to_s
+						  ])
+
+				# If all three arrays are empty or nil, pass execution to another thread
+				if (rsock.nil?||rsock.empty?) && (wsock.nil?||wsock.empty?) && (esock.nil?||esock.empty?)
+					Thread.pass
+					
+				else
+
+					### Handle socket errors
+					if !esock.nil? && esock.length.nonzero?
+						so_error = mySocket.getsockopt( SOL_SOCKET, SO_ERROR )
+						_debugMsg( 2, "Socket error: #{so_error.inspect}" )
+					end
+
+					### Read any input from the socket if it's ready
+					if !rsock.nil? && rsock.length.nonzero?
+						readData = mySocket.sysread( @@MTU )
+						_debugMsg( 5, "Read data in select loop (readData = '#{readData}', length = #{readData.length})." )
+						_handleRawInput( readData )
+					end
+
+					### Write any buffered output to the socket if we have
+					### output pending and the socket is writable
+					if !wsock.nil? && wsock.length.nonzero?
+						_debugMsg( 5, "Writing in select loop (@writebuffer = '#{@writeBuffer}')." )
+						@writeMutex.synchronize(Sync::EX) {
+							bytesWritten = mySocket.syswrite( @writeBuffer )
+							@writeBuffer[0 .. bytesWritten] = ''
+						}
+					end
+				end
+			end
+		end
+
+
+		### TODO: Possibly abstract the output buffer handling away from the
+		### ioLoop, too?
+
+
+		### (PROTECTED) METHOD: _handleRawInput( data )
+		### Handle the given raw input data which has just been read from the
+		### client socket.
+		def _handleRawInput( data )
+			@readBuffer += data
+			_debugMsg( 5, "Handling raw input (@readBuffer = '#{@readBuffer}', " +
+					  "length = #{@readBuffer.length})." )
+
+			unless @readBuffer.empty?
+				_debugMsg( 4, "Read buffer is non-empty. Trying to get input events from it." )
+				@readBuffer = _parseInputBuffer( @readBuffer )
+			end
+		end
+
+		
+		### (PROTECTED) METHOD: _parseInputBuffer( inputBuffer )
 		### Parse input events from the given raw buffer and return the
 		### (possibly) modified buffer after queueing any input events created.
-		def _parseRawInput( inputBuffer )
+		def _parseInputBuffer( inputBuffer )
 			newInputEvents = []
 
 			# Split input lines by CR+LF and strip whitespace before
@@ -214,10 +349,8 @@ module MUES
 			inputBuffer.gsub!( /^([^#{CR}#{LF}]*)#{CR}#{LF}?/ ) {|s|
 				_debugMsg( 5, "Read a line: '#{s}' (#{s.length} bytes)." )
 
-				#if ( s =~ /\w/ )
 					_debugMsg( 4, "Creating an input event for input = '#{s.strip}'" )
 					newInputEvents.push( InputEvent.new("#{s.strip}") )
-				#end
 				
 				""
 			}
