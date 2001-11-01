34b667817bb1492aa10389dfabbdebe11c42f26f
Michael Granger <ged@FaerieMUD.org>
2001-11-01 17:03:26 +0000

    - Updated documentation
    - Added debuggable mixin
    
    - Modified environment loading code to prepend the root dir onto the environment
      dir if it isn't preceeded by a '/'
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40220

--- lib/mues/environment.rb
+++ lib/mues/environment.rb
@@ -21,19 +21,74 @@ Environment - MUES Environment object class
 
 This is an abstract base class for MUES environment objects.
 
-Things which a environment must offer:
+== Classes
 
---- getAvailableRoles( aUser )
+=== MUES::Environment
+==== Class Methods
+
+--- MUES::Environment.atEngineShutdown( theEngine )
+
+    Clean up subsystems before engine shutdown
+
+--- MUES::Environment.atEngineStartup( theEngine )
+
+    Initialize subsystems after engine startup
+
+--- MUES::Environment.create( concreteClassName )
+
+    Load and instantiate the class specified by concreteClassName
+    and return it
+
+--- MUES::Environment.inherited( aSubClass )
+
+    Register the specified class with the list of child classes
+
+--- MUES::Environment.listEnvClasses()
+
+    Return an array of environment classes which have been loaded
+
+--- MUES::Environment.loadEnvClasses( config=MUES::Config )
+
+    Iterate over each file in the environments directory, loading
+    each one if it^s changed since last we loaded
+
+==== Public Method
+
+--- MUES::Environment#name
+
+    Return the name of the environment.
+
+--- MUES::Environment#description
+
+    Return the environment description.
+
+==== Protected Methods
+
+--- MUES::Environment#initialize( aName, aDescription )
+
+    Initialize an environment object with the specified name and description
+
+==== Abstract Methods
+
+--- MUES::Environment#getAvailableRoles( aUser )
 
     Returns an Array of MUES::Role objects that are available to the specified
     user.
 
---- getParticipantProxy( aUser, aRole )
+--- MUES::Environment#getParticipantProxy( aUser, aRole )
 
 	Connect the specified user to the environment in the specified role and
 	return a MUES::ParticipantProxy object if the connection is successful, or
 	raise a EnvironmentConnectFailed exception with an explanatory message
 	describing the failure if the connection could not be established.
+
+--- MUES::Environment#start
+
+    Start the environment instance.
+
+--- MUES::Environment#stop
+
+    Stop the environment instance.
 
 == Author
 
@@ -61,11 +116,11 @@ module MUES
 	def_exception :EnvironmentNameConflictError, "Environment name conflict error", Exception
 
 	### Environment abstract base class
-	class Environment < Object ; implements AbstractClass, Notifiable
+	class Environment < Object ; implements AbstractClass, Notifiable, Debuggable
 
 		### Class constants
-		Version = /([\d\.]+)/.match( %q$Revision: 1.7 $ )[1]
-		Rcsid = %q$Id: environment.rb,v 1.7 2001/09/26 12:41:22 deveiant Exp $
+		Version = /([\d\.]+)/.match( %q$Revision: 1.8 $ )[1]
+		Rcsid = %q$Id: environment.rb,v 1.8 2001/11/01 17:03:26 deveiant Exp $
 
 		### Class variables
 		@@ChildClasses = {}
@@ -82,6 +137,10 @@ module MUES
 				checkType( config, MUES::Config )
 				envdir = config["Environments"]["EnvironmentsDir"] or
 					raise Exception "No environments directory configured!"
+				if envdir !~ %r{^/}
+					debugMsg( 2, "Prepending rootdir '#{config['rootdir']}' to environments directory." )
+					envdir = File.join( config['rootdir'], envdir )
+				end
 
 				### Load all ruby source in the configured directory newer
 				### than our last load time. Each child will be registered
