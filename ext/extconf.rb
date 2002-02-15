0fe0d53b81da34551c1c05d27eb6eca9bfc1c2e7
Michael Granger <ged@FaerieMUD.org>
2002-02-15 07:33:24 +0000

    - Changed name to PolymorphicObject
    - Added rdoc and test targets
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40289

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,15 +1,30 @@
 #!/usr/bin/ruby
 # :nodoc: all
 #
-#	Extension configuration script for MonadicObject
-#	$Id: extconf.rb,v 1.2 2002/02/12 00:44:02 deveiant Exp $
+#	Extension configuration script for PolymorphicObject
+#	$Id: extconf.rb,v 1.3 2002/02/15 07:33:24 deveiant Exp $
 #
 #
 
 require "mkmf"
 
 $CFLAGS << " -Wall "
-dir_config( "MonadicObject" )
-create_makefile( "MonadicObject" )
+dir_config( "PolymorphicObject" )
+create_makefile( "PolymorphicObject" )
+
+File.open( "Makefile", "a" ) {|makefile|
+
+	makefile.puts <<-"EOF"
+
+RDOC=/usr/bin/rdoc
+
+rdoc: $(RDOC)
+	$(RDOC) --title "PolymorphicObject"
+
+test: $(DLLIB)
+	ruby test.rb
+
+	EOF
+}
 
 
