bf04b1bce3ca8b0616d3df6a4199e05da7336efe
Michael Granger <ged@FaerieMUD.org>
2002-03-01 11:17:07 +0000

    - Switched documentation systems from RD to RDoc.
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40304

--- docs/makedocs.rb
+++ docs/makedocs.rb
@@ -1,147 +1,79 @@
 #!/usr/bin/ruby
 #
 #	MUES Documentation Generation Script
-#	$Id: makedocs.rb,v 1.2 2001/12/06 13:38:25 red Exp $
+#	$Id: makedocs.rb,v 1.3 2002/03/01 11:17:07 deveiant Exp $
 #
-#	Copyright (c) 2001, The FaerieMUD Consortium.
+#	Copyright (c) 2001,2002 The FaerieMUD Consortium.
 #
 #	This is free software. You may use, modify, and/or redistribute this
 #	software under the terms of the Perl Artistic License. (See
 #	http://language.perl.com/misc/Artistic.html)
 #
 
-if $0 =~ /docs#{File::Separator}.*$/
-	baseDir = $0.gsub( /docs#{File::Separator}.*$/, '' )
-	$: << baseDir
-else
-	$: << '..'
+# Muck with the load path and the cwd
+$filename = __FILE__
+$basedir = File::expand_path( $0 ).sub( %r{/docs/makesitedocs.rb}, '' )
+unless $basedir.empty? || Dir.getwd == $basedir
+	$stderr.puts "Changing working directory from '#{Dir.getwd}' to '#$basedir'"
+	Dir.chdir( $basedir ) 
 end
 
-require "ftools"
-require "find"
-require "delegate"
-
+# Load modules
+require 'getoptlong'
+require 'rdoc/rdoc'
 require "utils"
 include UtilityFunctions
 
-$extendedHtml = false
-begin
-	require "rd/rd2html-ext-lib"
-	$extendedHtml = true
-rescue LoadError => e
-	puts "rd/rd2html-ext-lib unavailable, will not use extended HTML"
+opts = GetoptLong.new
+opts.set_options(
+	[ '--debug',	'-d',	GetoptLong::NO_ARGUMENT ],
+	[ '--verbose',	'-v',	GetoptLong::NO_ARGUMENT ],
+)
+
+$docsdir = "docs/html"
+$libdirs = %w{lib server}
+opts.each {|opt,val|
+	case opt
+
+	when '--debug'
+		$debug = true
+
+	when '--verbose'
+		$verbose = true
+
+	when '--upload'
+		$upload = true
+
+	end
+}
+
+
+header "Making documentation in #$docsdir from files in #{$libdirs.join(', ')}."
+
+flags = [
+	'--all',
+	'--inline_source',
+	'--main', 'lib/mues.rb',
+	'--include', 'docs',
+	'--op', $docsdir,
+	'--title', "Multi-User Environment Server (MUES)"
+]
+
+message "Running 'rdoc #{flags.join(' ')} #{$libdirs.join(' ')}'\n" if $verbose
+
+unless $debug
+	begin
+		r = RDoc::RDoc.new
+		r.document( flags + $libdirs  )
+	rescue RDoc::RDocError => e
+		$stderr.puts e.message
+		exit(1)
+	end
 end
 
-class Generator < SimpleDelegator
-
-	class HtmlDelegate
-		def initialize( docsDir )
-			@docsDir = docsDir
-			@targetDir = File.join( docsDir, "html" )
-			@generator = findProgram( 'rd2' ) or
-				raise RuntimeError, "You don't seem to have the rd2 program"
-			$stderr.puts( "Using extended HTML library" ) if $extendedHtml
-			@prepIsDone = false
-		end
-
-		def doPrep
-			File.makedirs( @targetDir, File.join(@targetDir, "stylesheets") )
-			File.copy( File.join(@docsDir, "stylesheets", "rd.css"),
-					   File.join(@targetDir, "stylesheets", "rd.css"),
-					   true )
-
-			@prepIsDone = true
-		end
-
-		# /usr/bin/rd -r html --with-css=/stylesheets/rd.css --html-lang=en \
-		#         -ohtml/MUES::Namespace \
-		#         --html-title=MUES::Namespace \
-		#         mues/Namespace.rb
-		def generateDocs( srcFile, outputName )
-			doPrep unless @prepIsDone
-			if $extendedHtml
-				system( @generator,
-						'-r', "rd/rd2html-ext-lib",
-						'--with-css=stylesheets/rd.css', '--html-lang=en',
-					    '--ref-extension', '--headline-secno',
-					    '--enable-br', '--native-inline', '--head-element',
-						"-o%s%s%s" % [ @targetDir, File::Separator, outputName ],
-						"--html-title=%s" % outputName,
-						srcFile ) or raise StandardError, "Failed rd2html-ext: #{$?}"
-			else
-				system( @generator,
-						'-r', "rd/rd2html-lib",
-						'--with-css=stylesheets/rd.css', '--html-lang=en',
-						"-o%s%s%s" % [ @targetDir, File::Separator, outputName ],
-						"--html-title=%s" % outputName,
-						srcFile ) or raise StandardError, "Failed rd2html: #{$?}"
-			end
-		end
-	end
-
-	class ManDelegate
-		def initialize( docsDir )
-			@docsDir = docsDir
-			@targetDir = File.join( docsDir, "man" )
-			@generator = findProgram( 'rd2' ) or
-				raise RuntimeError, "You don't seem to have the rd2 program"
-		end
-
-		def generateDocs( srcFile, outputName )
-			File.makedirs( @targetDir )
-			system( @generator,
-				    "-r", "rd/rd2man-lib",
-				    "-o%s%s%s" % [ @targetDir, File::Separator, outputName ],
-				    srcFile ) or raise StandardError, "Failed rd2man: #{$?}"
-		end
-	end
-
-	protected
-	def initialize( targetDir="docs", type="html" )
-		@targetDir = targetDir
-		raise TypeError, "No such documentation type '#{type}'" unless
-			Generator.const_defined? "#{type.capitalize}Delegate".intern
-		delegateClass = Generator.const_get "#{type.capitalize}Delegate".intern
-		@delegate = delegateClass.new( targetDir )
-		super( @delegate )
-	end
-	
-end
-
-if $0 == __FILE__
-	docsdir = File.dirname( File.expand_path($0) )
-	libdir = docsdir.sub( /docs/, "lib" )
-
-	header "Making documenatation in #{docsdir} from files in #{libdir}."
-
-	generators = [
-		Generator.new( docsdir, "html" ),
-		Generator.new( docsdir, "man" )
-	]
-
-	Dir[ "#{libdir}#{File::Separator}*" ].each {|subdir|
-		namespace = case subdir
-					when /mues/
-						'MUES'
-					when /tableadapter/
-						'TableAdapter'
-					when /metaclass/
-						'Metaclass'
-					else
-						subdir.sub( "#{libdir}#{File::Separator}", '' )
-					end
-
-		Find.find( subdir ) {|f|
-			if FileTest.file?( f ) and f =~ /\.rb$/
-				outputName = f.sub( /.*#{File::Separator}/, '' )
-				outputName.sub!( /\.rb$/, '' )
-				outputName = namespace + "::" + outputName
-
-				for g in generators
-					g.generateDocs( f, outputName )
-				end
-			end
-		}
-	}
-end
-
+# rdoc \
+#	--all \
+#	--inline_source \
+#	--main "lib/mues.rb" \
+#	--title "Multi-User Environment Server (MUES)" \
+#		lib server
--- docs/makesitedocs.rb
+++ docs/makesitedocs.rb
@@ -1,148 +1,96 @@
 #!/usr/bin/ruby
 #
 #	MUES Documentation Generation Script
-#	$Id: makesitedocs.rb,v 1.1 2001/12/05 18:05:07 deveiant Exp $
+#	$Id: makesitedocs.rb,v 1.2 2002/03/01 11:17:07 deveiant Exp $
 #
-#	Copyright (c) 2001, The FaerieMUD Consortium.
+#	Copyright (c) 2001,2002 The FaerieMUD Consortium.
 #
 #	This is free software. You may use, modify, and/or redistribute this
 #	software under the terms of the Perl Artistic License. (See
 #	http://language.perl.com/misc/Artistic.html)
 #
 
-if $0 =~ /docs#{File::Separator}.*$/
-	baseDir = $0.gsub( /docs#{File::Separator}.*$/, '' )
-	$: << baseDir
-else
-	$: << '..'
+# Muck with the load path and the cwd
+$filename = __FILE__
+$basedir = File::expand_path( $0 ).sub( %r{/docs/makesitedocs.rb}, '' )
+unless $basedir.empty? || Dir.getwd == $basedir
+	$stderr.puts "Changing working directory from '#{Dir.getwd}' to '#$basedir'"
+	Dir.chdir( $basedir ) 
 end
 
-require "ftools"
-require "find"
-require "delegate"
+$LOAD_PATH.unshift "docs/lib"
 
+# Load modules
+require 'getoptlong'
+require 'rdoc/rdoc'
 require "utils"
 include UtilityFunctions
 
-if require "rd/rd2html-ext-lib"
-	$extendedHtml = true
-else
-	$extendedHtml = false
-end
+opts = GetoptLong.new
+opts.set_options(
+	[ '--debug',	'-d',	GetoptLong::NO_ARGUMENT ],
+	[ '--verbose',	'-v',	GetoptLong::NO_ARGUMENT ],
+	[ '--upload',	'-u',	GetoptLong::NO_ARGUMENT ]
+)
 
-class Generator < SimpleDelegator
+$docsdir = "docs/html"
+$libdirs = %w{lib server}
+opts.each {|opt,val|
+	case opt
 
-	class HtmlDelegate
-		def initialize( docsDir )
-			@docsDir = docsDir
-			@targetDir = File.join( docsDir, "html" )
-			@generator = findProgram( 'rd2' ) or
-				raise RuntimeError, "You don't seem to have the rd2 program"
-			$stderr.puts( "Using extended HTML library" ) if $extendedHtml
-			@prepIsDone = false
-		end
+	when '--debug'
+		$debug = true
 
-		def doPrep
-			File.makedirs( @targetDir, File.join(@targetDir, "stylesheets") )
-			File.copy( File.join(@docsDir, "stylesheets", "rd.css"),
-					   File.join(@targetDir, "stylesheets", "rd.css"),
-					   true )
+	when '--verbose'
+		$verbose = true
 
-			@prepIsDone = true
-		end
+	when '--upload'
+		$upload = true
 
-		# /usr/bin/rd -r html --with-css=/stylesheets/rd.css --html-lang=en \
-		#         -ohtml/MUES::Namespace \
-		#         --html-title=MUES::Namespace \
-		#         mues/Namespace.rb
-		def generateDocs( srcFile, outputName )
-			doPrep unless @prepIsDone
-			if $extendedHtml
-				system( @generator,
-						'-r', "rd/rd2html-ext-lib",
-						'--with-css=/stylesheets/rd.css', '--html-lang=en',
-					    '--ref-extension', '--headline-secno',
-					    '--enable-br', '--native-inline', '--head-element',
-						"-o%s%s%s" % [ @targetDir, File::Separator, outputName ],
-						"--html-title=%s" % outputName,
-						srcFile ) or raise StandardError, "Failed rd2html-ext: #{$?}"
-			else
-				system( @generator,
-						'-r', "rd/rd2html-lib",
-						'--with-css=/stylesheets/rd.css', '--html-lang=en',
-						"-o%s%s%s" % [ @targetDir, File::Separator, outputName ],
-						"--html-title=%s" % outputName,
-						srcFile ) or raise StandardError, "Failed rd2html: #{$?}"
-			end
-		end
 	end
+}
 
-	class ManDelegate
-		def initialize( docsDir )
-			@docsDir = docsDir
-			@targetDir = File.join( docsDir, "man" )
-			@generator = findProgram( 'rd2' ) or
-				raise RuntimeError, "You don't seem to have the rd2 program"
-		end
 
-		def generateDocs( srcFile, outputName )
-			File.makedirs( @targetDir )
-			system( @generator,
-				    "-r", "rd/rd2man-lib",
-				    "-o%s%s%s" % [ @targetDir, File::Separator, outputName ],
-				    srcFile ) or raise StandardError, "Failed rd2man: #{$?}"
-		end
-	end
+header "Making documentation in #$docsdir from files in #{$libdirs.join(', ')}."
 
-	protected
-	def initialize( targetDir="docs", type="html" )
-		@targetDir = targetDir
-		raise TypeError, "No such documentation type '#{type}'" unless
-			Generator.const_defined? "#{type.capitalize}Delegate".intern
-		delegateClass = Generator.const_get "#{type.capitalize}Delegate".intern
-		@delegate = delegateClass.new( targetDir )
-		super( @delegate )
-	end
-	
-end
+flags = [
+	'--all',
+	'--inline_source',
+	'--main', 'lib/mues.rb',
+	'--fmt', 'myhtml',
+	'--include', 'docs',
+	'--template', 'faeriemud',
+	'--op', $docsdir,
+	'--title', "Multi-User Environment Server (MUES)"
+]
 
-if $0 == __FILE__
-	docsdir = File.dirname( File.expand_path($0) )
-	libdir = docsdir.sub( /docs/, "lib" )
+message "Running 'rdoc #{flags.join(' ')} #{$libdirs.join(' ')}'\n" if $verbose
 
-	header "Making documenatation in #{docsdir} from files in #{libdir}."
-
-	generators = [
-		Generator.new( docsdir, "html" )
-	]
-
-	Dir[ "#{libdir}#{File::Separator}*" ].each {|subdir|
-		namespace = case subdir
-					when /mues/
-						'MUES'
-					when /tableadapter/
-						'TableAdapter'
-					when /metaclass/
-						'Metaclass'
-					else
-						subdir.sub( "#{libdir}#{File::Separator}", '' )
-					end
-
-		Find.find( subdir ) {|f|
-			if FileTest.file?( f ) and f =~ /\.rb$/
-				outputName = f.sub( /.*#{File::Separator}/, '' )
-				outputName.sub!( /\.rb$/, '' )
-				outputName = namespace + "::" + outputName
-
-				for g in generators
-					g.generateDocs( f, outputName )
-				end
-			end
-		}
-	}
-
-	for g in generators
-		g.generateDocs( "docs/MUES.rd", "MUES" )
+unless $debug
+	begin
+		r = RDoc::RDoc.new
+		r.document( flags + $libdirs  )
+	rescue RDoc::RDocError => e
+		$stderr.puts e.message
+		exit(1)
 	end
 end
 
+if $upload
+	if ENV['HOSTNAME'] =~ /faeriemud/i
+		header "Uploading new docs snapshot to oberon.FaerieMUD.org."
+		unless $debug
+			system( "tar -C docs/html -cf - . | ssh oberon 'tar -C /www/mues.FaerieMUD.org/public/rdoc -xvf -'" )
+		end
+	end
+end
+
+# rdoc \
+#	--all \
+#	--inline_source \
+#	--main "lib/mues.rb" \
+#	--fmt myhtml \
+#	--include docs \
+#	--template faeriemud \
+#	--title "Multi-User Environment Server (MUES)" \
+#		lib server
