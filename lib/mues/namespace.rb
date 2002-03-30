730777ae5c8f78f1e623862368017a61bba85db3
Michael Granger <ged@FaerieMUD.org>
2002-03-30 19:16:24 +0000

    - Converted documentation to RDoc.
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40350

--- lib/mues/namespace.rb
+++ lib/mues/namespace.rb
@@ -1,58 +1,69 @@
 #!/usr/bin/ruby
-###########################################################################
-=begin
+# 
+# This file contains the Metaclass::Namespace class. Instances of this class are
+# convenience objects that allow the definition of metaclasses to be made in a
+# namespace separate from the main Ruby namespace.
+# 
+# == Synopsis
+# 
+#   require "metaclass/Namespace"
+# 
+#   ns = MetaClass::Namespace.new( "SomeName" )
+#   ns.addClass( metaclass )
+# 
+#   eval "#{ns}"
+# 
+# == Rcsid
+# 
+# $Id: namespace.rb,v 1.3 2002/03/30 19:16:24 deveiant Exp $
+# 
+# == Authors
+# 
+# * Michael Granger <ged@FaerieMUD.org>
+# 
+#:include: COPYRIGHT
+#
+#---
+#
+# Please see the file COPYRIGHT for licensing details.
+#
 
-=Namespace.rb
+require 'metaclass/Constants'
 
-== Name
+module Metaclass
 
-Namespace - A namespace metaclass
+	autoload :Class, 'metaclass/Class'
 
-== Synopsis
+	### A namespace metaclass
+	class Namespace
 
-  require "metaclass/Namespace"
+		Version = /([\d\.]+)/.match( %q$Revision: 1.3 $ )[1]
+		Rcsid = %q$Id: namespace.rb,v 1.3 2002/03/30 19:16:24 deveiant Exp $
 
-  ns = MetaClass::Namespace.new( "SomeName" )
-  ns.addClass( metaclass )
-
-  eval "#{ns}"
-
-== Description
-
-Instances of this class are convenience objects that allow the definition of
-metaclasses to be made in a namespace separate from the main Ruby namespace.
-
-== Author
-
-Michael Granger <((<ged@FaerieMUD.org|URL:mailto:ged@FaerieMUD.org>))>
-
-Copyright (c) 2001 The FaerieMUD Consortium. All rights reserved.
-
-This module is free software. You may use, modify, and/or redistribute this
-software under the terms of the Perl Artistic License. (See
-http://language.perl.com/misc/Artistic.html)
-
-=end
-###########################################################################
-
-module MetaClass
-	class Namespace < Object
-
-		Version = /([\d\.]+)/.match( %q$Revision: 1.2 $ )[1]
-		Rcsid = %q$Id: namespace.rb,v 1.2 2001/05/14 12:36:59 deveiant Exp $
-
-		attr_accessor :name, :classes
-
+		### Create and return a new namespace object with the specified +name+.
 		def initialize( name )
 			@name = name
 			@classes = []
 		end
 
+
+		######
+		public
+		######
+
+		# The name of the namespace
+		attr_accessor :name
+
+		# The Array of classes within the namespace
+		attr_accessor :classes
+
+		### Add the specified metaclass classes to the namespace.
 		def addClasses( *classes )
 			@classes.push classes
 			@classes.flatten!
 		end
 
+		### Return the namespace as evalable code
 		def to_s
 			"module #{@name}\n" + @classes.sort.reverse.collect {|k| k.classDefinition(true,true)}.join("\n") + "end"
 		end
