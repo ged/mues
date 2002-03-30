88c1db201e11b6c7d021185f975b42ba8e3336af
Michael Granger <ged@FaerieMUD.org>
2002-03-30 19:43:23 +0000

    - Converted documentation to RDoc.
    - Parameter-code building is now delegated to Parameter objects.
    
    - Added accessor auto-generation for visible attributes.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40351

--- lib/mues/operation.rb
+++ lib/mues/operation.rb
@@ -1,80 +1,95 @@
 #!/usr/bin/ruby
-###########################################################################
-=begin
+# 
+# This file contains the Metaclass::Operation metaclass, which is used to add
+# operations to instances of Metaclass::Class.
+# 
+# == Synopsis
+# 
+#   require "metaclass/Operation"
+#   require "metaclass/Parameter"
+# 
+#   op = Operation.new( "initialize", <<-'EOM' )
+#     @name = name
+#     @attrib = attrib
+# 
+#     puts "Initialized"
+#   end
+#   EOM
+# 
+#   op << Metaclass::Parameter::new( 'name', String, "defaultName" )
+#   op << Metaclass::Parameter::new( 'attrib', [String, Numeric, IO], $stderr )
+# 
+#   classObject << op
+# 
+#   # -or-
+# 
+#   eval "#{op.methodDefinition}"
+# 
+# == Rcsid
+# 
+# $Id: operation.rb,v 1.3 2002/03/30 19:43:23 deveiant Exp $
+# 
+# == Authors
+# 
+# * Michael Granger <ged@FaerieMUD.org>
+# 
+#:include: COPYRIGHT
+#
+#---
+#
+# Please see the file COPYRIGHT for licensing details.
+#
 
-=Operation.rb
+require 'metaclass/Constants'
+require 'metaclass/Parameter'
 
-== Name
+module Metaclass
 
-Operation - An operation metaclass
-
-== Synopsis
-
-  require "metaclass/Operation"
-
-  op = Operation.new( "initialize", <<-"EOM" )
-    @name = name
-    @attrib = attrib
-
-    puts "Initialized"
-  end
-  EOM
-
-  op.addArgument( name, String, "defaultName" )
-  op.addArgument( attrib, [String, Numeric, IO], $stderr )
-
-  classObject.addOperation( "initialize", op )
-
-  # -or-
-
-  eval "#{op.methodDefinition}"
-
-== Description
-
-Instances of this class are object which are useful adding methods to instances
-of the "Class" metaclass.
-
-== Author
-
-Michael Granger <((<ged@FaerieMUD.org|URL:mailto:ged@FaerieMUD.org>))>
-
-Copyright (c) 2001 The FaerieMUD Consortium. All rights reserved.
-
-This module is free software. You may use, modify, and/or redistribute this
-software under the terms of the Perl Artistic License. (See
-http://language.perl.com/misc/Artistic.html)
-
-=end
-###########################################################################
-
-module MetaClass
-
+	### Operation metaclass error exception class.
 	class OperationError < ScriptError; end
-	class UndefinedValue ; end
 
-	class Operation < Object
+	### Parameter collision error: raised when two parameters with the same name
+	### are added to a Metaclass::Operation. If the second argument to
+	### <tt>raise</tt> is a Metaclass::Parameter object, an appropriate error
+	### message will be built based on its name.
+	class ParameterCollisionError < OperationError
+
+		# Set the default error message based on the type of argument.
+		def initialize( msg=nil ) # :nodoc:
+			case msg
+			when String
+				super( msg )
+
+			when Metaclass::Parameter
+				super( "Cannot add a second '#{msg.name}' parameter." )
+
+			else
+				super( "Cannot add two like-named parameters to an operation." )
+			end
+		end
+	end
+
+
+	### An operation metaclass
+	class Operation
 
 		### Constants
-		VISIBILITY_PUBLIC		= 1
-		VISIBILITY_PROTECTED	= 2
-		VISIBILITY_PRIVATE		= 3
+		DEFAULT_CODE = "    raise StandardError, \"Unimplemented method.\""
+		DEFAULT_VISIBILITY		= Visibility::PUBLIC
+		DEFAULT_SCOPE			= Scope::INSTANCE
 
-		SCOPE_CLASS				= 1
-		SCOPE_INSTANCE			= 2
+		COMMENT_WRAP_WIDTH		= 77
 
-		DEFAULT_CODE = "raise StandardError, \"Unimplemented method.\""
-		DEFAULT_VISIBILITY		= VISIBILITY_PUBLIC
-		DEFAULT_SCOPE			= SCOPE_INSTANCE
+		Version = /([\d\.]+)/.match( %q$Revision: 1.3 $ )[1]
+		Rcsid = %q$Id: operation.rb,v 1.3 2002/03/30 19:43:23 deveiant Exp $
 
-		UNDEF = UndefinedValue.new
-
-		Version = /([\d\.]+)/.match( %q$Revision: 1.2 $ )[1]
-		Rcsid = %q$Id: operation.rb,v 1.2 2001/05/14 12:36:59 deveiant Exp $
-
-		attr_accessor :name, :description, :arguments, :code, :visibility, :scope
-
-		### METHOD: initialize( name )
-		def initialize( name, code = DEFAULT_CODE, scope = DEFAULT_SCOPE, visibility = DEFAULT_VISIBILITY )
+		
+		### Return a new Operation with the specified name. If the code argument
+		### is given, it becomes the content of the operation. The scope
+		### argument specifies if the operation is to be a method of the class
+		### to which it is applied, or to instances of that class. The
+		### visibility argument specifies from where it can be called.
+		def initialize( name, code=DEFAULT_CODE, scope=DEFAULT_SCOPE, visibility=DEFAULT_VISIBILITY )
 			@name = name
 			@code = code
 			@visibility = visibility
@@ -83,180 +98,201 @@ module MetaClass
 			@description = nil
 		end
 
-		### METHOD: <=>( otherOperation )
-		def <=>( otherOperation )
-			val = @scope <=> otherOperation.scope
-			val = @visibility <=> otherOperation.visibility	if val == 0
-			val = @name <=> otherOperation.name				if val == 0
-			val = self.id <=> otherOperation.id				if val == 0
-			
-			return val
+
+		######
+		public
+		######
+
+		# The name of the operation
+		attr_accessor :name
+
+		# The human-readable description of the operation (documentation)
+		attr_accessor :description
+
+		# The array of arguments the operations takes
+		attr_accessor :arguments
+
+		# The implementation-specific code of the operation
+		attr_accessor :code
+
+		# The visibility of the operation (One of the Metaclass::Visibility
+		# constants)
+		attr_accessor :visibility
+
+		# The scope of the operation (One of the Metaclass::Scope constants)
+		attr_accessor :scope
+
+
+		### Append operator: Appends a parameter to the list of parameters this
+		### operation currently has and returns the receiver.
+		def <<( param )
+			addArgument( param )
+			return self
 		end
 
-		### METHOD: addArgument( name, validType = nil, default = nil )
-		def addArgument( name, validType = nil, default = UNDEF )
-			raise TypeError, "validType argument must be one of [Class,String,Array], not a #{validType.inspect}" unless
-				[::Class,Class,String,Array,NilClass].find {|k| k === validType}
 
-			unless validType == nil || default == UNDEF
-				defaultMatchesType = false
-				if validType.is_a?( Array )
-					defaultMatchesType = validType.find {|k| k === default}
-				elsif validType.is_a?( String )
-					defaultMatchesType = default.type.name == validType
-				else
-					defaultMatchesType = validType === default
-				end
+		### <tt>Comparable</tt> interface method. Returns -1, 0, or 1 if this
+		### operation should sort higher, the same, or lower than the specified
+		### <tt>otherOperation</tt>. In truth, this method will only return '0'
+		### if <tt>otherOperation</tt> is the same as the receiver.
+		def <=>( otherOperation )
+			return (@scope <=> otherOperation.scope).nonzero? ||
+				(@visibility <=> otherOperation.visibility).nonzero? ||
+				(@name <=> otherOperation.name).nonzero? ||
+				self.id <=> otherOperation.id
+		end
 
-				raise OperationError, "Default for argument does not match validType specification" unless defaultMatchesType
+
+		### Add an argument to this operation. The +param+ argument can either
+		### be a Metaclass::Parameter object, or a parameter name. If +param+ is
+		### a Parameter object, the <tt>validTypes</tt> and <tt>default</tt>
+		### arguments will be supplied as arguments to
+		### Metaclass::Parameter#new. The new Parameter object will be appended
+		### to the current #arguments, if any. A
+		### Metaclass::ParameterCollisionError will be raised if the new
+		### parameter has the same name as one that has already been added.
+		def addArgument( param, validTypes=nil, default=nil )
+			case param
+			when Metaclass::Parameter
+				# no-op
+
+			when String, Symbol
+				param = Metaclass::Parameter.new( param, validTypes, default )
+
+			else
+				raise TypeError, 
+					"Illegal first argument type. Expected a Metaclass::Parameter or a String, " +
+					"got a #{param.type.name}."
 			end
 
-			arg = { "name" => name }
-			arg["type"] = validType if validType
-			arg["default"] = default if default != UNDEF
+			# Detect parameter collision
+			raise ParameterCollisionError, param if
+				@arguments.detect {|arg| arg.name == param.name}
 
-			@arguments.push arg
+			@arguments.push param
 		end
 
-		### METHOD: methodDefinition( methodName )
-		def methodDefinition( methodName=@name, includeComment=true )
-			argList = _methodArgList()
-			argCheckCode = _argCheckBlock()
 
+		### Delete the specified argument from the operation. The +param+
+		### argument can be either a Metaclass::Parameter object or a String
+		### which will be taken as the name of the argument to remove.
+		def delArgument( param )
+			case param
+			when Metaclass::Parameter
+				@arguments.delete( param )
+
+			when String
+				@arguments.delete_if {|arg| arg.name == param}
+
+			else
+				raise TypeError, "No implicit conversion to Metaclass::Parameter from #{param.type.name}"
+			end
+		end
+
+
+		### Return the operation as a method with the specified
+		### <tt>methodName</tt>. If <tt>includeComment</tt> is <tt>true</tt>,
+		### the method declaration will be have a leading comment block with the
+		### operation's <tt>description</tt>.
+		def methodDefinition( methodName=@name, includeComment=true )
+			argList = buildMethodArgList()
+			argCheckCode = buildArgCheckBlock()
+
+			# Set up the definition line and add any parameters
 			defLine = "#{methodName}"
 			defLine += "( #{argList} )" if argList != ''
 
-			trimmedCode = @code #.gsub( /^\s*/, "" )
+			# Normalize the indent of the code
+			code = @code
 
+			# Build the array of source
 			definition = []
-			definition << methodComment( defLine ) if includeComment
+			definition << buildMethodComment() if includeComment
 			if argCheckCode != ''
-				definition << [ "def #{defLine}", argCheckCode, trimmedCode, "end" ]
+				definition << "def #{defLine}" << argCheckCode << "\n" << @code << "end"
 			else
-				definition << [ "def #{defLine}", trimmedCode, "end" ]
+				definition << "def #{defLine}" << @code << "end"
 			end
 
-			if @scope == SCOPE_CLASS
+			# Add scope and visibility code
+			if @scope == Scope::CLASS
 				definition.unshift "class <<self"
 				definition.push "end"
 			else
 				case @visibility
-				when VISIBILITY_PROTECTED
+				when Visibility::PROTECTED
 					definition << "protected :#{methodName}"
-				when VISIBILITY_PRIVATE
+				when Visibility::PRIVATE
 					definition << "private :#{methodName}"
 				end
 			end
 
 			definition << ""
-
 			return definition
 		end
 
-		### METHOD: methodComment( definitionLine, name=@name )
-		def methodComment( defLine, name = @name )
-			modifiers = []
 
-			case @visibility
-			when VISIBILITY_PROTECTED
-				modifiers << "PROTECTED"
-			when VISIBILITY_PRIVATE
-				modifiers << "PRIVATE"
-			end
-
-			if @scope == SCOPE_CLASS
-				modifiers << "CLASS"
-			end
-
-			if modifiers.length > 0
-				return "### (#{modifiers.join(' ')}) METHOD: #{defLine}"
-			else
-				return "### METHOD: #{defLine}"
-			end
+		### Build and return a comment block for the operation derived from the
+		### <tt>description</tt>.
+		def buildMethodComment
+			desc = @description || '(Undocumented)'
+			desc = wrap( desc ) if desc.length > COMMENT_WRAP_WIDTH
+			return desc.collect {|str| "# " + str}.join("\n")
 		end
 
-	  protected
 
-		### (PROTECTED) METHOD: _methodArgList
-		def _methodArgList
+
+		#########
+		protected
+		#########
+
+		### :TODO: Write a text-formatter library (extends String, maybe?) to
+		### wrap, indent, etc.
+
+		### Wrap the specified +text+ to the specified +width+. Returns an
+		### Array of strings of +width+ or less characters long.
+		def wrap( text, width=COMMENT_WRAP_WIDTH )
+			workingText = text.dup
+			newText = []
+
+			while workingText.length > width
+				i = width
+				until workingText[i,1] =~ /\s/ do
+					raise RuntimeError, "Text is not wrappable to the specified width." if i < 1
+					i -= 1
+				end 
+				newText << workingText[ 0, i ]
+				workingText[ 0, i + 1 ] = ''
+			end
+
+			newText << workingText
+			return newText
+		end
+
+
+		### Build and return the method's argument list code.
+		def buildMethodArgList
 			argStrings = []
 			defaultedArgSeen = false
 
 			arguments.each_with_index {|arg, i|
-				raise TypeError, "Illegal arg #{i}: Must be a Hash" unless arg.is_a?( Hash )
-				raise OperationError, "Argument #{i} is missing a name" unless arg["name"]
-
-				if defaultedArgSeen || arg.has_key?("default")
-					argStrings.push "%s = %s" % [ arg["name"], arg["default"].inspect ]
+				if defaultedArgSeen || arg.default
+					argStrings.push "%s=%s" % [ arg.name, (arg.default || "nil") ]
 					defaultedArgSeen = true
 				else
-					argStrings.push arg["name"]
+					argStrings.push arg.name
 				end
 			}
 
 			return argStrings.join(', ')
 		end
 
-		### (PROTECTED) METHOD: _argCheckBlock
-		def _argCheckBlock
-			checkCode = []
-			arguments.each_with_index {|arg, i|
-				raise TypeError, "Illegal arg #{i}: Must be a Hash" unless arg.is_a?( Hash )
-				raise OperationError, "Argument #{i} is missing a name" unless arg["name"]
 
-				next unless arg.key?("type")
-
-				str = _argCheckCode( arg )
-				checkCode.push str if str
-			}
-
-			return checkCode.join("\n\t")
+		### Build and return the method's argument type-checking code
+		def buildArgCheckBlock
+			return "    " + arguments.collect {|arg| arg.buildCheckCode}.flatten.join("\n    ") + "\n"
 		end
 
-		### (PROTECTED) METHOD: _argCheckCode( argname )
-		def _argCheckCode( arg )
-			raise TypeError, "Arg to build string must be a Hash" unless arg.is_a?( Hash )
-			raise ArgumentError, "Arg doesn't have a name" unless arg.has_key?("name")
-			
-			return nil unless arg.has_key?( "type" )
-
-			case arg["type"]
-			when String
-				return "raise TypeError, \"argument '%s' must be a %s\" unless %s.is_a?( %s )" % [
-					arg['name'],
-					arg['type'],
-					arg['name'],
-					arg['type']
-				]
-			when Array
-				if arg["type"].length > 1
-					typeArray = "[#{arg['type'].collect {|t| t.name}.join(', ') }]"
-
-					code = "unless #{typeArray}.find {|k| k === #{arg['name']}.type}"
-					code += "\n\t\traise TypeError, \"#{arg['name']} must be one of #{typeArray}\""
-					code += "\n\tend"
-				else
-					code = "raise TypeError, \"argument '%s' must be a %s\" unless %s.is_a?( %s )" % [
-						arg['name'],
-						arg['type'][0].name,
-						arg['name'],
-						arg['type'][0].name
-					]
-				end
-
-				return code
-			when ::Class
-				return "raise TypeError, \"argument '%s' must be a %s\" unless %s.is_a?( %s )" % [
-					arg['name'],
-					arg['type'].name,
-					arg['name'],
-					arg['type'].name
-				]
-			else
-				raise TypeError, "Unhandled type '#{arg['type'].type.inspect}' specified for #{arg['name']}"
-			end
-
-		end
 
 	end
 end
@@ -264,19 +300,23 @@ end
 
 
 if $0 == __FILE__
-	op = MetaClass::Operation.new( "unimplemented" )
-	op2 = MetaClass::Operation.new( "doSomething", <<-"EOF" )
-	iterations.times do
-		puts "Look, ma, I'm doin' something!"
-	end
+	op = Metaclass::Operation.new( "unimplemented" )
+	op.description = "A simple test method with no code."
 
-	puts "Okay. That's enough for now. Here's my second arg: \#{secondArg}"
+	op2 = Metaclass::Operation.new( "doSomething", <<-"EOF" )
+		iterations.times do
+			puts "Look, ma, I'm doin' something!"
+		end
+
+		puts "Okay. That's enough for now. Here's my second arg: \#{secondArg}"
 	EOF
 
+	op2.description = "A more complex testing method"
 	op2.addArgument( "iterations", Integer )
-	op2.addArgument( "secondArg", String, "the Default Value" )
+	op2.addArgument( "secondArg", String, '"the Default Value"' )
 	op2.addArgument( "unused" )
 	op2.addArgument( "unused2", [ Array, Class, String, Fixnum ] )
+
 
 	puts "Method one:"
 	puts op.methodDefinition
