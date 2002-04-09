57299e96265c7ff92ce0101d27df330dda1352ab
Michael Granger <ged@FaerieMUD.org>
2002-04-09 07:02:27 +0000

    - Removed old 'prelude' cruft from the documentation for   #initialize. (red@FaerieMUD.org)
    - Fixed the '=' bug in the generated code: '@thing= = val'.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40372

--- lib/mues/mutatoroperation.rb
+++ lib/mues/mutatoroperation.rb
@@ -12,7 +12,7 @@
 # 
 # == Rcsid
 # 
-# $Id: mutatoroperation.rb,v 1.2 2002/04/06 16:53:06 deveiant Exp $
+# $Id: mutatoroperation.rb,v 1.3 2002/04/09 07:02:27 deveiant Exp $
 # 
 # == Authors
 # 
@@ -33,19 +33,22 @@ module Metaclass
 	class MutatorOperation < Metaclass::Operation
 
 		### Class constants
-		Version = /([\d\.]+)/.match( %q$Revision: 1.2 $ )[1]
-		Rcsid = %q$Id: mutatoroperation.rb,v 1.2 2002/04/06 16:53:06 deveiant Exp $
+		Version = /([\d\.]+)/.match( %q$Revision: 1.3 $ )[1]
+		Rcsid = %q$Id: mutatoroperation.rb,v 1.3 2002/04/09 07:02:27 deveiant Exp $
 
 		### Create a new MutatorOperation object that sets the instance variable
 		### of the name specified by <tt>sym</tt> (a <tt>Symbol</tt> or
 		### <tt>String</tt>).If the <tt>validTypes</tt> argument is specified (a
 		### Class, a String with the name of a Class, or an Array of either
 		### Class or Name objects), the mutator will also contains code to check
-		### for a valid value via <tt>kind_of?</tt> before the prelude. The
-		### <tt>scope</tt> and <tt>visibility</tt> arguments are passed to
+		### for a valid value via <tt>kind_of?</tt>. The <tt>scope</tt> and
+		### <tt>visibility</tt> arguments are passed to
 		### Metaclass::Operation#new.
 		def initialize( sym, validTypes=nil, scope=nil, visibility=nil )
+
+			# Stringify the symbol and strip any trailing equals
 			sym = sym.id2name if sym.is_a? Symbol
+			sym.gsub!( /=$/, '' )
 
 			# Assemble the mutator code
 			code = <<-END_CODE
@@ -53,7 +56,7 @@ module Metaclass
 			END_CODE
 
 			# Call the superclass's initializer
-			super( sym, code, scope, visibility )
+			super( "#{sym}=", code, scope, visibility )
 
 			# Add the 'val' argument
 			self.addArgument( :val, validTypes )
