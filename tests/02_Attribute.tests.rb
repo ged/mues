8666572decd52bfb295ae44134aadeec167f82a4
Michael Granger <ged@FaerieMUD.org>
2002-04-09 07:08:29 +0000

    - Modified to use MUES::TestCase instead of the base Test::Unit one.
    - Swapped Association and Attribute tests.
    
    - Split out all test blocks into test methods.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40375

--- tests/02_Attribute.tests.rb
+++ tests/02_Attribute.tests.rb
@@ -1,163 +1,198 @@
 #!/usr/bin/ruby -w
 
-require 'metaclass/Constants'
-require 'metaclass/Attribute'
+begin
+	require 'muesunittest'
+rescue
+	require '../muesunittest'
+end
 
-require 'test/unit'
+require 'metaclasses'
 
-### Log tests
-module Metaclass
-	class AttributeTestCase < Test::Unit::TestCase
 
-		include Metaclass::Scope
-		include Metaclass::Visibility
+class AttributeTestCase < MUES::TestCase
 
-		### Test instantiation with various arguments
-		def test_Instantiate
-			attrObj = nil
+	include Metaclass::Scope
+	include Metaclass::Visibility
 
-			# No-args, should raise an ArgumentError
-			assert_raises( ArgumentError ) { Metaclass::Attribute.new }
-			assert_raises( TypeError ) { Metaclass::Attribute.new(14) }
+	### Test no-arg instantiation
+	def test_00NoArg_Instantiate
+		attrObj = nil
 
-			# One-arg
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new("name") }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_equal INSTANCE, attrObj.scope
-			assert_equal PUBLIC, attrObj.visibility
-			assert_equal [], attrObj.validTypes
+		# No-args, should raise an ArgumentError
+		assert_raises( ArgumentError ) { Metaclass::Attribute.new }
+		assert_raises( TypeError ) { Metaclass::Attribute.new(14) }
+	end
 
-			# Two-arg, single validType
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_equal INSTANCE, attrObj.scope
-			assert_equal PUBLIC, attrObj.visibility
-			assert_equal [String], attrObj.validTypes
+	# One-arg
+	def test_01OneArg_Instantiate
+		attrObj = nil
 
-			# Two-arg, multiple class validTypes
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new("gameid",[String,"Numeric",Array,"IO"]) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "gameid", attrObj.name
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new(:gameid,[String,"Numeric",Array,"IO"]) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "gameid", attrObj.name
-			assert_equal INSTANCE, attrObj.scope
-			assert_equal PUBLIC, attrObj.visibility
-			assert_instance_of Array, attrObj.validTypes
-			assert_not_nil attrObj.validTypes.detect {|obj| obj == String}
-			assert_not_nil attrObj.validTypes.detect {|obj| obj == "Numeric"}
-			assert_not_nil attrObj.validTypes.detect {|obj| obj == Array}
-			assert_not_nil attrObj.validTypes.detect {|obj| obj == "IO"}
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new("name") }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_equal INSTANCE, attrObj.scope
+		assert_equal PUBLIC, attrObj.visibility
+		assert_equal [], attrObj.validTypes
+	end
 
-			# Three-arg, instance scope
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,INSTANCE) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,INSTANCE) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_equal INSTANCE, attrObj.scope
-			assert_equal PUBLIC, attrObj.visibility
+	# Two-arg, single validType
+	def test_02TwoArgValidType_Instantiate
+		attrObj = nil
 
-			# Three-arg, class scope
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,CLASS) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,CLASS) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_equal CLASS, attrObj.scope
-			assert_equal PUBLIC, attrObj.visibility
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_equal INSTANCE, attrObj.scope
+		assert_equal PUBLIC, attrObj.visibility
+		assert_equal [String], attrObj.validTypes
+	end
 
-			# Four-arg, public
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,INSTANCE,PUBLIC) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,INSTANCE,PUBLIC) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_equal INSTANCE, attrObj.scope
-			assert_equal PUBLIC, attrObj.visibility
+	# Two-arg, multiple class validTypes
+	def test_03TwoArgMultipleValidType_Instantiate
+		attrObj = nil
 
-			# Three-arg, protected
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,INSTANCE,PROTECTED) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,INSTANCE,PROTECTED) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_equal INSTANCE, attrObj.scope
-			assert_equal PROTECTED, attrObj.visibility
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new("gameid",[String,"Numeric",Array,"IO"]) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "gameid", attrObj.name
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new(:gameid,[String,"Numeric",Array,"IO"]) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "gameid", attrObj.name
+		assert_equal INSTANCE, attrObj.scope
+		assert_equal PUBLIC, attrObj.visibility
+		assert_instance_of Array, attrObj.validTypes
+		assert_not_nil attrObj.validTypes.detect {|obj| obj == String}
+		assert_not_nil attrObj.validTypes.detect {|obj| obj == "Numeric"}
+		assert_not_nil attrObj.validTypes.detect {|obj| obj == Array}
+		assert_not_nil attrObj.validTypes.detect {|obj| obj == "IO"}
+	end
 
-			# Three-arg, private
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,INSTANCE,PRIVATE) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,INSTANCE,PRIVATE) }
-			assert_instance_of Metaclass::Attribute, attrObj
-			assert_equal "name", attrObj.name
-			assert_equal INSTANCE, attrObj.scope
-			assert_equal PRIVATE, attrObj.visibility
-		end
+	# Three-arg, instance scope
+	def test_04ThreeArgInstance_Instantiate
+		attrObj = nil
 
-		### Test the Comparable interface operators/methods
-		def test_Comparable
-			aInstance	= Metaclass::Attribute.new( "a", nil, INSTANCE )
-			bInstance	= Metaclass::Attribute.new( "b", nil, INSTANCE )
-			cInstance	= Metaclass::Attribute.new( "c", nil, INSTANCE )
-			aClass		= Metaclass::Attribute.new( "a", nil, CLASS )
-			cClass		= Metaclass::Attribute.new( "c", nil, CLASS )
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,INSTANCE) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,INSTANCE) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_equal INSTANCE, attrObj.scope
+		assert_equal PUBLIC, attrObj.visibility
+	end
 
-			# Name sort
-			assert aInstance < bInstance
+	# Three-arg, class scope
+	def test_05ThreeArgClass_Instantiate
+		attrObj = nil
 
-			# Scope sort
-			assert aInstance < aClass
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,CLASS) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,CLASS) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_equal CLASS, attrObj.scope
+		assert_equal PUBLIC, attrObj.visibility
+	end
 
-			# Scope sort
-			assert cInstance < aClass
+	# Four-arg, public
+	def test_06FourArgPublic_Instantiate
+		attrObj = nil
 
-			# Name sort
-			assert aClass < cClass
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,INSTANCE,PUBLIC) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,INSTANCE,PUBLIC) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_equal INSTANCE, attrObj.scope
+		assert_equal PUBLIC, attrObj.visibility
+	end
 
-			# Between method
-			assert bInstance.between?( aInstance, cInstance )
-			assert !bInstance.between?( cInstance, cClass )
-			
-		end
+	# Four-arg, protected
+	def test_07FourArgProtected_Instantiate
+		attrObj = nil
 
-		def test_AccessorOp
-			attrObj = Metaclass::Attribute.new( "key", [String,Numeric] )
-			op = nil
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,INSTANCE,PROTECTED) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,INSTANCE,PROTECTED) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_equal INSTANCE, attrObj.scope
+		assert_equal PROTECTED, attrObj.visibility
+	end
 
-			assert_nothing_raised { op = attrObj.makeAccessorOp }
-			assert_kind_of Metaclass::Operation, op
-			assert_equal "key", op.name
-			assert_equal INSTANCE, op.scope
-			assert_equal PUBLIC, op.visibility
+	# Four-arg, private
+	def test_08FourArgPrivate_Instantiate
+		attrObj = nil
 
-		end
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new("name",String,INSTANCE,PRIVATE) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_nothing_raised { attrObj = Metaclass::Attribute.new(:name,String,INSTANCE,PRIVATE) }
+		assert_instance_of Metaclass::Attribute, attrObj
+		assert_equal "name", attrObj.name
+		assert_equal INSTANCE, attrObj.scope
+		assert_equal PRIVATE, attrObj.visibility
+	end
 
-		def test_MutatorOp
-			attrObj = Metaclass::Attribute.new( "key", [String,Numeric] )
-			op = nil
+	### Test the Comparable interface operators/methods
+	def test_09Comparable
+		aInstance	= Metaclass::Attribute.new( "a", nil, INSTANCE )
+		bInstance	= Metaclass::Attribute.new( "b", nil, INSTANCE )
+		cInstance	= Metaclass::Attribute.new( "c", nil, INSTANCE )
+		aClass		= Metaclass::Attribute.new( "a", nil, CLASS )
+		cClass		= Metaclass::Attribute.new( "c", nil, CLASS )
 
-			assert_nothing_raised { op = attrObj.makeMutatorOp }
-			assert_kind_of Metaclass::Operation, op
-			assert_equal "key=", op.name
-			assert_equal INSTANCE, op.scope
-			assert_equal PUBLIC, op.visibility
+		# Name sort
+		assert aInstance < bInstance
 
-		end
+		# Scope sort
+		assert aInstance < aClass
 
-	end # class AttributeTestCase
-end # module Metaclass
+		# Scope sort
+		assert cInstance < aClass
+
+		# Name sort
+		assert aClass < cClass
+
+		# Between method
+		assert bInstance.between?( aInstance, cInstance )
+		assert !bInstance.between?( cInstance, cClass )
+		
+	end
+
+	### Test the addition of accessors when adding an attribute to a class
+	def test_10AccessorOp
+		attrObj = Metaclass::Attribute.new( "key", [String,Numeric] )
+		op = nil
+
+		assert_nothing_raised { op = attrObj.makeAccessorOp }
+		assert_kind_of Metaclass::Operation, op
+		assert_equal "key", op.name
+		assert_equal INSTANCE, op.scope
+		assert_equal PUBLIC, op.visibility
+
+	end
+
+	### Test the addition of mutators when adding an attribute to a class
+	def test_11MutatorOp
+		attrObj = Metaclass::Attribute.new( "key", [String,Numeric] )
+		op = nil
+
+		assert_nothing_raised { op = attrObj.makeMutatorOp }
+		assert_kind_of Metaclass::Operation, op
+		assert_equal "key=", op.name
+		assert_equal INSTANCE, op.scope
+		assert_equal PUBLIC, op.visibility
+
+	end
+
+end # class AttributeTestCase
 
