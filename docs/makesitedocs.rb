9e7cb1ad0a5cb8933f6d10a202c4267a5a1007aa
Michael Granger <ged@FaerieMUD.org>
2002-05-16 03:39:09 +0000

    - Made --upload take an argument that specifies upload target.
    - Run RDoc in quiet mode unless --verbose is specified.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40408

--- docs/makesitedocs.rb
+++ docs/makesitedocs.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby
 #
 #	MUES Documentation Generation Script
-#	$Id: makesitedocs.rb,v 1.4 2002/03/31 18:34:20 deveiant Exp $
+#	$Id: makesitedocs.rb,v 1.5 2002/05/16 03:39:09 deveiant Exp $
 #
 #	Copyright (c) 2001,2002 The FaerieMUD Consortium.
 #
@@ -23,14 +23,15 @@ $LOAD_PATH.unshift "docs/lib"
 # Load modules
 require 'getoptlong'
 require 'rdoc/rdoc'
-require "utils"
+# require 'rdoc/parsers/parse_faeriemud_rb'
+require 'utils'
 include UtilityFunctions
 
 opts = GetoptLong.new
 opts.set_options(
 	[ '--debug',	'-d',	GetoptLong::NO_ARGUMENT ],
 	[ '--verbose',	'-v',	GetoptLong::NO_ARGUMENT ],
-	[ '--upload',	'-u',	GetoptLong::NO_ARGUMENT ]
+	[ '--upload',	'-u',	GetoptLong::REQUIRED_ARGUMENT ]
 )
 
 $docsdir = "docs/html"
@@ -45,7 +46,7 @@ opts.each {|opt,val|
 		$verbose = true
 
 	when '--upload'
-		$upload = true
+		$upload = val
 
 	end
 }
@@ -64,7 +65,9 @@ flags = [
 	'--title', "Multi-User Environment Server (MUES)"
 ]
 
+flags += [ '--quiet' ] unless $verbose
 message "Running 'rdoc #{flags.join(' ')} #{$libdirs.join(' ')}'\n" if $verbose
+
 
 unless $debug
 	begin
@@ -77,13 +80,51 @@ unless $debug
 end
 
 if $upload
-	if ENV['HOSTNAME'] =~ /faeriemud/i
-		header "Uploading new docs snapshot to oberon.FaerieMUD.org."
-		unless $debug
-			system( "tar -C docs/html -cf - . | ssh oberon 'tar -C /www/mues.FaerieMUD.org/public/rdoc -xvf -'" )
+	header "Uploading new docs snapshot to #$upload."
+	case $upload
+	
+	# SSH target
+	when %r{^ssh://(.*)}
+		target = $1
+		if target =~ %r{^([^/]+)/(.*)}
+			host, path = $1, $2
+			path = "/" + path unless path =~ /^(\/|\.)/
+			cmd = "tar -C docs/html -cf - . | ssh #{host} 'tar -C #{path} -xvf -'"
+			unless $debug
+				system( cmd )
+			else
+				message "Would have uploaded using the command:\n    #{cmd}\n\n"
+			end
+		else
+			error "--upload ssh://host/path"
 		end
+	else
+		targetdir = $upload
+		targetdir.gsub!( %r{^file://}, '' )
+
+		File.makedirs targetdir, true
+		Dir["docs/html/**/*"].each {|file|
+			fname = file.gsub( %r{docs/html/}, '' )
+			if File.directory? file
+				unless $debug
+					File.makedirs File.join(targetdir, fname), true
+				else
+					message %{File.makedirs %s, true\n} % File.join(targetdir, fname)
+				end
+			else
+				unless $debug
+					File.install( file, File.join(targetdir, fname), 0444, true )
+				else
+					message %{File.install( %s, %s, 0444, true )\n} % [
+						file,
+						File.join(targetdir, fname),
+					]
+				end
+			end
+		}
 	end
 end
+
 
 # rdoc \
 #	--all \
