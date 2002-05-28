f993b4bb9df19ee65bdfec15f91035c0a71c7584
Michael Granger <ged@FaerieMUD.org>
2002-05-28 16:44:16 +0000

    - Removed old target cruft.
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40439

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,7 +2,7 @@
 # :nodoc: all
 #
 #	Extension configuration script for StorableObject
-#	$Id: extconf.rb,v 1.4 2002/05/28 15:55:09 stillflame Exp $
+#	$Id: extconf.rb,v 1.5 2002/05/28 16:44:16 deveiant Exp $
 #
 #
 
@@ -11,20 +11,5 @@ require "mkmf"
 $CFLAGS << " -Wall "
 dir_config( "StorableObject" )
 create_makefile( "StorableObject" )
-
-File.open( "Makefile", "a" ) {|makefile|
-
-	makefile.puts <<-"EOF"
-
-RDOC=/usr/bin/rdoc
-
-rdoc: $(RDOC)
-	$(RDOC) --title "StorableObject"
-
-test: $(DLLIB)
-	ruby test.rb
-
-	EOF
-}
 
 
