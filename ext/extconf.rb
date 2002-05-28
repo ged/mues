34cbb2107595503772125eee9c72b2a42fcb77f2
Michael Granger <ged@FaerieMUD.org>
2002-05-28 17:40:08 +0000

    - Undid rename, as Storable is distinct from Polymorphic after all. Both   MUES::StorableObject and MUES::ShallowReference need to inherit polymorphism.
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40447

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/ruby
 # :nodoc: all
 #
-#	Extension configuration script for StorableObject
-#	$Id: extconf.rb,v 1.5 2002/05/28 16:44:16 deveiant Exp $
+#	Extension configuration script for MUES::PolymorphicObject
+#	$Id: extconf.rb,v 1.6 2002/05/28 17:40:08 deveiant Exp $
 #
 #
 
 require "mkmf"
 
 $CFLAGS << " -Wall "
-dir_config( "StorableObject" )
-create_makefile( "StorableObject" )
+dir_config( "PolymorphicObject" )
+create_makefile( "PolymorphicObject" )
 
 
