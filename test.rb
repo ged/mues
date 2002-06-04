13810cffc9860fa2deed0c67f569dcbf689381f8
Michael Granger <ged@FaerieMUD.org>
2002-06-04 15:20:11 +0000

    - Added selection of tests by pattern
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40488

--- test.rb
+++ test.rb
@@ -7,17 +7,34 @@
 $:.unshift "lib", "tests"
 
 require 'find'
+require 'test/unit'
 require 'test/unit/testsuite'
 require 'test/unit/ui/console/testrunner'
+
+patterns = []
+ARGV.each {|pat| patterns << Regexp::new( pat )}
+
+requires = []
 
 ### Load all the tests from the tests dir
 Find.find("tests") {|file|
 	Find.prune if file =~ /^.'/ or file =~ /~$/
 	Find.prune if file =~ /TEMPLATE/
 	next if File.stat( file ).directory?
+
+ 	unless patterns.empty?
+ 		Find.prune unless patterns.find {|pat| pat =~ file}
+ 	end
+
 	next unless file =~ /\.tests.rb$/
 	require "#{file}"
+	requires << file
 }
+
+$stderr.puts "Required #{requires.length} files."
+unless patterns.empty?
+	$stderr.puts "[" + requires.sort.join( ", " ) + "]"
+end
 
 class MUESTests
 	class << self
