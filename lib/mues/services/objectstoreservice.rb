5446cd7a45c5bdbf4b15c9edf9712721e2bc841e
Michael Granger <ged@FaerieMUD.org>
2002-06-04 12:53:03 +0000

    -  Fully-qualified class/module references
    - Fixed requires section
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40479

--- lib/mues/services/objectstoreservice.rb
+++ lib/mues/services/objectstoreservice.rb
@@ -1,66 +1,45 @@
 #!/usr/bin/ruby -w
 #
-# This file contains the class for the ObjectStore service, as it will be
-# used by MUES.
-#
-# == Copyright
-#
-# Copyright (c) 2002 FaerieMUD Consortium, all rights reserved.
-# 
-# This is Open Source Software.  You may use, modify, and/or redistribute 
-# this software under the terms of the Perl Artistic License, a copy of which
-# should have been included in this distribution (See the file Artistic). If
-# it was not, a copy of it may be obtained from
-# http://language.perl.com/misc/Artistic.html or
-# http://www.faeriemud.org/artistic.html).
-# 
-# THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES,
-# INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND
-# FITNESS FOR A PARTICULAR PURPOSE.
+# This file contains the MUES::ObjectStoreService class, which is an ObjectStore
+# service for MUES.
 #
 # == Synopsis
 #
-#   require "mues"
-#   require "mues/Service"
-#   require "mues/Events"
-#   require "ObjectStoreService"
+#    require 'mues/services/ObjectStoreService'
 #
-#   event = MUES::ObjectStoreGCEvent.new
-#   eventQueue.enqueue( event )
 #
-#   # for more info on specific usage of this service, see the postscript or uml
-#   # files included.
-#   
-# == Caveats
-#
-#   All objects stored must inherit from the class StorableObject, for
-#   the required ability to be a shallow reference.
 #
 # == Authors
 #
 # * Martin Chase <stillflame@FaerieMUD.org>
+#
+#:include: COPYRIGHT
+#
+#---
+#
+# Please see the file COPYRIGHT for licensing details.
 #
 
 require "mues"
 require "mues/Service"
 require "mues/Events"
 require "mues/Exceptions"
-require "ObjectStoreEvents"
-require "ObjectStore"
-require "ObjectStoreGC"
+require "mues/ObjectStore"
 
 module MUES
 
 	### Class for the ObjectStore system's Service interface to MUES
-	class ObjectStoreService < Service
-		include Event::Handler
+	class ObjectStoreService < MUES::Service
+
+		# Include the default event dispatcher method
+		include MUES::Event::Handler
 
 		### Initialize a new ObjectStoreService object, passing in a hash of
 		### values with the following keys:
 		def initialize()
 			@objectStores = []
 			@name = "ObjectStoreService"
-			registerHandlerForEvents( GetServiceAdapterEvent )
+			registerHandlerForEvents( MUES::GetServiceAdapterEvent )
 		end
 
 		### Class methods
@@ -79,7 +58,8 @@ module MUES
 		### Check to see if anyone wants an ObjectStore
 		def _handleGetServiceAdapterEvent (event)
 			if (event.name == @name)
-				@objectStores << ObjectStore.new(*(event.args))
+				store = ObjectStore.new(*(event.args))
+				@objectStores << 
 				event.callback.call( @objectStores[-1] )
 			end
 			return []
