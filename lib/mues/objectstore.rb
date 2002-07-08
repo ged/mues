ffd831b3897acc854793e4761051d42ee1bd4956
Michael Granger <ged@FaerieMUD.org>
2002-07-08 14:59:53 +0000

    - Updated to reflect namechange of the MemoryManager.
    - Reworked constructor/assembly methods
    
    - Reworked interface for consistency
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40519

--- lib/mues/objectstore.rb
+++ lib/mues/objectstore.rb
@@ -6,7 +6,7 @@
 # The ObjectStore is a factory/wrapper class that provides a front end for
 # combining a Strategy for object persistance (a MUES::ObjectStore::Backend)
 # and, optionally, a one for swapping disused objects (mostly) out of memory
-# temporarily via a MUES::ObjectStore::GarbageCollector.
+# temporarily via a MUES::ObjectStore::MemoryManager.
 #   
 # == Caveats/Requirements
 #
@@ -15,12 +15,21 @@
 # * Multiple ObjectStores alive in the same ruby process must have distinct
 #   names.
 #
+# == Specifying a Backend or MemoryManager
+#
+# <em>Not yet done.</em>
+#
 # == Synopsis
 #
 #   require "mues/ObjectStore"
 #   require "mues/StorableObject"
 #
-#   $store = MUES::ObjectStore.load( "test_store" )
+#   $store = MUES::ObjectStore::create(
+#		:backend	=> "BerkeleyDB",
+#		:memmgr			=> "PMOS",
+#		:name		=> "mystore"
+#	)
+#
 #   objs = []
 #   ids = []
 #   40.times do
@@ -29,13 +38,10 @@
 #       ids << newObj.objectStoreID
 #   end
 #
-#   $store.store( objs )
-#   #...or with garbage collection
-#   $store.register( objs )
-#   #...and watch as they disappear ^_^
+#   $store.store( *objs )
+#   $store.retrieve_readonly {|obj| puts obj.attr}
 #
-#   object = $store.retrieve( ids[12] )
-#   object.read_only_do {|x| puts x}
+#	object = $store.retrieve( "objid" )
 #   object.attribute_A = 3.14159262546
 #   $store.store( object )
 #
@@ -43,7 +49,7 @@
 #
 # == Version
 #
-#  $Id: objectstore.rb,v 1.28 2002/06/04 07:03:47 deveiant Exp $
+#  $Id: objectstore.rb,v 1.29 2002/07/08 14:59:53 deveiant Exp $
 # 
 # == Authors
 #
@@ -66,10 +72,6 @@ require 'mues/Exceptions'
 require 'mues/StorableObject'
 require 'mues/ObjectSpaceVisitor'
 
-require 'mues/os-extensions/FlatfileBackend'
-require 'mues/os-extensions/NullGarbageCollector'
-
-
 module MUES
 
     # Exception class for ObjectStore errors
@@ -78,34 +80,26 @@ module MUES
 
 	# The ObjectStore provides a mechanism for object persistance and,
 	# optionally, a way of swapping disused objects (mostly) out of memory
-	# temporarily via a GarbageCollector.
+	# temporarily via a MemoryManager.
     class ObjectStore < MUES::Object
 
 		include MUES::TypeCheckFunctions
 
-		### Default de/serializing proc
-		DefaultDumperProc = Proc.new {|obj|
-			case obj
-			when String
-				Marshal.load( obj )
-			when StorableObject
-				Marshal.dump( obj )
-			else
-				raise ObjectStoreException, "Cannot serialize a #{obj.class.name}"
-			end
-		}
+		### Class constants
+		Version	= %q$Revision: 1.29 $
+		RcsId	= %q$Id: objectstore.rb,v 1.29 2002/07/08 14:59:53 deveiant Exp $
 
-		### The default GarbageCollector class
-		DefaultGcClass = MUES::ObjectStore::NullGarbageCollector
+		# The default MemoryManager class
+		DefaultMemMgr = "Null"
 
-		### The default Backend class
-		DefaultBackendClass = MUES::ObjectStore::FlatfileBackend
+		# The default Backend class
+		DefaultBackend = "Flatfile"
 
-		### The default GarbageCollectorVisitor class
-		DefaultGcVisitorClass = MUES::ObjectSpaceVisitor
+		# The default MemoryManager Visitor class
+		DefaultMemMgrVisitor = MUES::ObjectSpaceVisitor
 
-		### The name of the subdirectory to load default backends and
-		### garbagecollector classes from (relative to $LOAD_PATH)
+		# The name of the subdirectory to load default backends and
+		# memorymanager classes from (relative to $LOAD_PATH)
 		BackendDir = 'mues/os-extensions'
 
 
@@ -122,149 +116,169 @@ module MUES
 		### Disallow new() to prevent multiple instances of the same-named store
 		private_class_method :new
 
-		### Load the specified catalog, and returns the ObjectStore attached to
-		### it.  Will create a new ObjectStore if file doesn't exist.
+		### Factory method: Combine the specified backend, memory manager,
+		### and visitor objects together in a MUES::ObjectStore object. The
+		### <tt>args</tt> are a Hash with the following keys:
 		###
-		### Arguments:
-        ### name::         the store identifier
-        ### indexes::      an Array of symbols/strings for index methods
-        ### dump_undump::  the Proc to give an object to get a string and to give a
-        ###                string to get an object.
-		### gcClass::      The name of the garbage collector class to use.
-		### backendClass:: The name of the backend class to use.
-		def ObjectStore.load( name, indexes = [], dump_undump = DefaultDumperProc,
-							 gcClass = DefaultGcClass, backendClass = DefaultBackendClass,
-							 gcVisitor = DefaultGcVisitorClass, garbageCollectorArgs = {} )
+        ### [:name]
+		###   A name (String) that acts as the store's unique
+		###   identifier. Calling #load twice with the same name will return the
+		###   previous one instantiated. <strong>Required</strong>.
+		### [:backend]
+		###   The name of the MUES::ObjectStore::Backend derivative to use, the
+		###   class object itself, or an instance of a backend.  See the
+		###   "Specifying a Backend or MemoryManager" section of
+		###   lib/mues/ObjectStore.rb for more information.
+		### [:memmgr]
+		###   The name of the MUES::ObjectStore::MemoryManager derivative to
+		###   use, the class object itself, or an instance of a memory
+		###   manager. See the "Specifying a Backend or MemoryManager" section
+		###   of lib/mues/ObjectStore.rb for more information.
+        ### [:indexes]
+		###   An Array of symbols/strings for index methods. Index methods are
+		###   methods which are to be used as index values to facilitate fast
+		###   retrieval of objects from the store by lookups on values instead
+		###   of by an id. See #lookup for more about how this works.
+		### [:visitor]
+		###   The MUES::ObjectSpaceVisitor derivative instance or class to use
+		###   for traversing the objectspace contained in the store.
+		### [:config]
+		###   A configuration hash that contains one or more of the following
+		###   keys. The value is used to configure the various parts of the
+		###   objectstore at instantiation. The configuration values will be
+		###   ignored if the corresponding part is specified by passing an
+		###   instance instead of a Class or a name.
+		###   [:memmgr]
+		###      If specified, the value will be passed as the last argument to
+		###      the MemoryManager on instantiation.
+		###   [:backend]
+		###      If specified, the value will be passed as the last argument to
+		###      the Backend on instantiation.
+		###   [:visitor]
+		###      If specified, the value will be passed as the last argument to
+		###      the Visitor on instantiation.
+		def self.create( args )
+			TypeCheckFunctions::checkResponse( args, :[] )
 
-			# Return either the instance with the specified name, or a new store
-			# if there isn't one yet loaded.
-			return @@instances[ name ] ||= ObjectStore.new( name, indexes, dump_undump,
-														    gcClass, backendClass,
-														    gcVisitor,
-														    garbageCollectorArgs )
+			config = args[:config] || {}
+
+			# Check the name argument
+			name = args[:name].to_s
+			TypeCheckFunctions::checkType( name, String )
+			return @@instances[ name ] if @@instances.has_key?( name )
+
+			# Check the indexes
+			indexes = args[:indexes] || []
+			TypeCheckFunctions::checkType( indexes, Array )
+			TypeCheckFunctions::checkEachType( indexes, String, Symbol )
+
+			# Build the list of objects we need for the store
+			visitor	= ObjectSpaceVisitor::create( args[:visitor], config[:visitor] )
+			backend	= Backend::create( args[:backend] || DefaultBackend, indexes,
+									   config[:backend] )
+			memmgr	= MemoryManager::create( args[:memmgr] || DefaultMemMgr, backend, config[:memmgr] )
+
+			# Return the new store after adding it to the instance list
+			return @@instances[ name ] = new( name, memmgr, backend, visitor )
 		end
 
+
+		### Assemble an ObjectStores according to the specified <tt>config</tt>
+		### object, which must be a MUES::Config::ObjectStoreSection.
+		def self.createFromConfig( config )
+			checkType( config, MUES::Config::ObjectStoreSection )
+
+			# Make a Hash out of all the construction arguments
+			configHash = {
+				:name => config['name'],
+				:backend => config.backend,
+				:memmgr => config.memoryManager,
+				:config => config.argHash,
+			}
+
+			# Visitor element is optional, so don't add it if it's not defined.
+			configHash[:visitor] = config.visitor if config.visitor
+
+			return self.create( configHash )
+		end
 
 
 		### Initializes a new ObjectStore
 		###
-		### Arguments:
-        ### name::         the store identifier
-        ### indexes::      an Array of symbols/strings for index methods
-        ### dump_undump::  the Proc to give an object to get a string and to give a
-        ###                string to get an object.
-		### gcClass::      The name of the garbage collector class to use.
-		### backendClass:: The name of the backend class to use.
-		### gcVisitor::    An instance of MUES::ObjectSpaceVisitor
-		###                or one of its derivative. If one is not provided, an
-		###                instance of the DefaultGcVisitorClass is provided.
-		def initialize( name, indexes, dump_undump, gcClass, backendClass,
-					    gcVisitor, garbageCollectorArgs ) # :notnew:
-			checkType( dump_undump, Proc, Method )
-			checkType( gcClass, Class )
-			checkType( backendClass, Class )
-
+        ### [name]
+		###   The store identifier
+		### [memmgr]
+		###   A MUES::ObjectStore::MemoryManager object.
+		### [backend]
+		###   A MUES::ObjectStore::Backend object.
+		### [visitor]
+		###   A MUES::ObjectSpaceVisitor object that will be used by the
+		###   MemoryManager to traverse the objects that have been fetched
+		###   from the store.
+		def initialize( name, memmgr, backend, visitor ) # :notnew:
 			super()
 
-			@name			= name
-			@indexes		= indexes
-			@dump_undump	= dump_undump
 			@mutex			= Sync.new
-			@database		= Backend::create( backendClass, self, name, dump_undump, indexes )
-			@gc				= GarbageCollector::create( gcClass, self )
+			@backend		= backend
+			@memmgr			= memmgr
+			@closed			= false
 
-			# Add accessors for the specified indexes
-			add_index_methods( *indexes )
-
-			# Start the garbage collector either with the visitor object
-			# specified, or an instance of the default
-			gcVisitor ||= DefaultGcVisitorClass::new
-			@gc.start( gcVisitor )
-
-			return self
+			@memmgr.start( visitor )
 		end
 
-
-		#########
-		protected
-		#########
-
-		### Auto-genererate methods for retrieving objects using the index names
-		### provided.  Each index must have a corresponding method on the
-		### objects to be stored.
-		###
-		### Methods created:
-		### [<tt>retrieve_by_<em>index</em></tt>] 
-		###   make a MUES::ShallowReference for the specified object.
-		### [<tt>_retrieve_by_<em>index</em></tt>]
-		###   grab the object, looking for the value provided first, then the
-		###   id.
-		### [<tt>_retrieve_all_by_<em>index</em></tt>]
-		###   grabs all objects whose indexing method returns the value
-		###   provided.
-		def add_index_methods ( *indexes )
-			indexes.flatten!
-			indexes.each {|ind|
-				ind_str = ind.to_s
-				ObjectStore.class_eval <<-END
-				
-				def retrieve_shallow_by_#{ind_str}(id)
-					register IndexedShallowReference.new( id, self, #{ind_str} )
-				end
-			
-				def retrieve_by_#{ind_str}(id, val)
-					@database.retrieve_by_index( id, #{ind_str} )
-				end
-
-				def retrieve_all_by_#{ind_str}(val)
-					register @database.lookup( {#{ind_str} -> val} )
-				end
-
-				END
-			}
-		end
-
-
+		
 		######
 		public
 		######
 
-		# the name of the catalog file
+		# The name of the ObjectStore
 		attr_reader :name
 
-		# the Proc to control serialization and it's reverse, deserialization
-		attr_reader :dump_undump
-
-		# a hash of A_Index objects, keyed by their names
-		attr_reader :indexes
-
-
-		### Stores the specified <tt>objects</tt> into the ObjectStore.
-		def store ( *objects )
-			@database.store( *objects )
+	
+		# Returns <tt>true</tt> if the objectstore has been closed.
+		def closed?
+			@closed
 		end
 
-		### Registers the specified <tt>objects</tt> with the garbage collector
-		### (so that they can be kept track of and swapped out of memory when
-		### needed). If the object is not already stored in the ObjectStore, it
-		### will be stored when it is swapped.
-		def register (*objects)
-			@gc.register(*objects)
+		
+		### Stores the specified <tt>objects</tt> into the ObjectStore after
+		### registering them with the MemoryManager.
+		def store( *objects )
+			@memmgr.register( *objects )
+			self.put( *objects )
+		end
+		
+
+		### Fetches and returns the MUES::StorableObject specifed by the given
+		### <tt>ids</tt>, after registering the objects with the
+		### MemoryManager.
+		def retrieve( *ids )
+			ids.collect {|id| @memmgr[id] ||= @backend.fetch(id)}
 		end
 
-		### Removes the specified objects from the ObjectStore after
-		### unregistering them.
+
+		### Fetch an Array of all objects stored in the ObjectStore.
+		def retrieve_all
+			objs = @backend.retrieve_all
+			return objs.collect {|obj| @memmgr[ obj.objectStoreID ] ||= obj.awaken}
+		end
+		alias :retrieveAll :retrieve_all
+
+		
+		### Given a hash of index keys and values, fetch and return each object
+		### that matches all of the specified pairs.
+		def lookup( index_pairs )
+			objs = @backend.lookup( index_pairs )
+			return objs.collect {|obj| @memmgr[ obj.objectStoreID ] ||= obj.awaken}
+		end
+
+
+		### Removes the specified objects from the ObjectStore.
 		def remove( *objects )
-			@database.remove( *objects )
+			@memmgr.unregister( *objects )
+			@backend.remove( *objects )
 		end
 
-		### Unregisters the specified <tt>objects</tt> with the garbage
-		### collector. The object will thenafter never be swapped out of memory
-		### (unless it is re-registed, of course), but the last version of the
-		### object will remain stored in the ObjectStore.
-		def unregister( *objects )
-			@gc.unregister( *objects )
-		end
 		
 		### Preload the @active_objects with all objects that match the given
 		### index name/value provided, or all objects if no arguments are used.
@@ -273,77 +287,119 @@ module MUES
 		### [index_hash]
 		###   Optionally, a hash of index to value(s) as per #lookup. Defaults
 		###   to loading all objects in the store.
-		def pre_load ( index_hash = Hash.new )
+		def preload( index_hash = Hash.new )
 			objs = ( (! index_hash.empty?) ? lookup(index_hash) : retrieve_all() )
-			@gc.register(objs)
+			@memmgr.register( objs )
 		end
+		alias_method :pre_load, :preload
+
 
 		### Allows momentary access to the object from the database, by calling
 		### this method and supplying a block.  No changes to the object made in
 		### the block will be written to the database.
-		def do_read_only( id )
-			obj = retrieve( id ).dup
+		def fetch_read_only( id )
+			raise ArgumentError, "Called without a block" unless
+				block_given?
+			obj = @memmgr[id] || self.fetch( id ).dup
 			return yield( obj )
 		end
+		alias :fetchReadOnly :fetch_read_only
+		alias :do_read_only :fetch_read_only
+		alias :doReadOnly :fetch_read_only
 
+		
 		### Closes the database.
 		def close
-			@gc.shutdown
-			@database.close
-			@database = nil
-			@gc = nil
+			objects = @memmgr.shutdown
+			@backend.store( *objects )
+			@backend.close
 		end
 
-		### Fetches and returns the MUES::StorableObject specifed by the given
-		### <tt>id</tt>.
-		def retrieve( id )
-			@gc[id] ||= @database[id]
-		end
-
-		### Gets and returns a MUES::ShallowReference to the object specified by
-		### <tt>id</tt> out of the ObjectStore.
-		def retrieve_shallow( id )
-			@gc[id] ||= ShallowReference.new( id, self )
-		end
-
-		### Given a hash of index keys and values, fetch and return each object
-		### that matches all of the specified pairs.
-		def lookup( index_pairs )
-			@database.lookup( index_pairs )
-		end
-
-		### Fetch an Array of all objects stored in the ObjectStore.
-		def retrieve_all
-			@database.retrieve_all
-		end
 
 		### Returns <tt>true</tt> if an object with the specified <tt>id</tt>
 		### exists in the ObjectStore.
-		def exists? ( id )
-			@database.exists?( id )
+		def exists?( id )
+			@backend.exists?( id )
 		end
+
 
 		### Returns <tt>true</tt> if the ObjectStore is empty.
 		def empty? 
-			@table.nitems == 0
+			@backend.nitems == 0
 		end
-		
-		### Returns <tt>true</tt> if the ObjectStore is currently open.
-		def open? 
-			@table ? true : false
-		end
+
 		
 		### Returns the number of objects stored in the ObjectStore.
-		def entries 
-			@table.nitems
+		def nitems
+			@backend.nitems
 		end
-		alias :size :entries
-		alias :count :entries
+		alias :entries :nitems
+		alias :size :nitems
+		alias :count :nitems
+
 		
-		### Removes (unregisters) all objects from the ObjectStore.
+		### Removes all objects from the ObjectStore.
 		def clear
-			@table.clear
+			@backend.clear
 		end
+
+
+
+		#########
+		protected
+		#########
+		
+		### Fetches and returns the MUES::StorableObjects specified by the given
+		### <tt>ids</tt> from the backend.
+		def fetch( *ids )
+			@backend.retrieve( *ids ).collect {|obj|
+				if obj.nil? then nil else obj.awaken end
+			}
+		end
+
+
+		### Puts the specified MUES::StorableObjects into the backend datastore.
+		def put( *objs )
+			checkEachType( objs, MUES::StorableObject )
+			@backend.store( objs.collect {|obj| obj.lull} )
+		end
+
+
+		### Auto-generate singleton methods on the fly for retrieving objects
+		### via an index.
+		###
+		### Methods that can be created:
+		### [<tt>retrieve_by_<em>index</em>( value )</tt>] 
+		###   Look up objects by the specified <tt>index</tt> and <tt>value</tt>
+		###   and return them.
+		def method_missing( sym, *args )
+			methodName = sym.to_s
+
+			# Look for a method pattern we know how to auto-define. If we find
+			# one, add it to the instance.
+			case methodName
+			when /^retrieve_by_(\w+)$/
+				idx = $1
+				return super( sym, *args ) unless @indexes.has_key?( idx )
+				code = %Q{
+					def self.#{methodName}( val )
+						@backend.retrieve_by_index( :#{idx}, val )
+					end
+				}
+
+			# Otherwise, just delegate the call to our parent's method_missing
+			else
+				return super( sym, *args )
+			end
+
+			# Evaluate the code, look up the new method, and call it with the
+			# arguments we got.
+			self.instance_eval code
+			raise RuntimeError, "Method definition for '#{sym.to_s}' failed." if 
+				method( methodName ).nil?
+			self.method( sym ).call( *args )
+		end
+
 
 
     end # class ObjectStore
