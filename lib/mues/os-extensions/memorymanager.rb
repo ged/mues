7419e7d023d02a472ca97de9ba7fa621b1eef66f
Michael Granger <ged@FaerieMUD.org>
2002-08-01 01:26:08 +0000

    - Added .derivativeDirs FactoryMethods callback.
    - Renamed objectStoreID method to objectStoreId.
    
    - Added a #clear method.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40577

--- lib/mues/os-extensions/memorymanager.rb
+++ lib/mues/os-extensions/memorymanager.rb
@@ -13,7 +13,7 @@
 # 
 # == Rcsid
 # 
-# $Id: memorymanager.rb,v 1.2 2002/07/09 15:09:25 deveiant Exp $
+# $Id: memorymanager.rb,v 1.3 2002/08/01 01:26:08 deveiant Exp $
 # 
 # == Authors
 # 
@@ -28,13 +28,14 @@
 
 require 'hashslice'
 require 'sync'
+require 'pp'
 
 require 'mues'
 require 'mues/Exceptions'
 require 'mues/ObjectSpaceVisitor'
 
 module MUES
-	class ObjectStore < MUES::Object
+	class ObjectStore
 
 		### This class is the abstract base class for MUES::ObjectStore
 		### memory-management Strategy classes. Derivatives of this class should
@@ -44,6 +45,16 @@ module MUES
 
 			include MUES::TypeCheckFunctions, MUES::FactoryMethods
 
+			### Class methods
+
+			# Returns the directory objectstore extensions live under (part of
+			# the FactoryMethods interface)
+			def self.derivativeDirs
+				return 'mues/os-extensions'
+			end
+
+
+			### Initializer
 
 			### Create and return a new MemoryManager with the specified
 			### <tt>backend</tt> (a MUES::ObjectStore::Backend derivative), and
@@ -52,7 +63,7 @@ module MUES
 				super()
 				
 				@backend		= backend
-				@activeObjects	= Hash::new
+				@activeObjects	= {}
 				@mutex			= Sync::new
 				@managerThread	= nil
 				@running		= false
@@ -74,7 +85,8 @@ module MUES
 			### registered with the MemoryManager.
 			def []( *ids )
 				@mutex.synchronize( Sync::SH ) {
-					@activeObjects[ *ids ]
+					# @activeObjects[ *ids ]
+					ids.collect {|id| @activeObjects[id]}
 				}
 			end
 
@@ -91,9 +103,9 @@ module MUES
 						begin
 							managerThreadRoutine( visitor )
 						rescue Reload
-							# :TODO: Log?
+							self.log.info( "Reloading #{self.inspect}" )
 						rescue Shutdown
-							# :TODO: Log?
+							self.log.info( "Shutting down #{self.inspect}" )
 						end
 						@running = false
 					}
@@ -101,34 +113,48 @@ module MUES
 			end
 
 
-			# :TODO: This may need a non-interruptive way of reloading to ensure
-			# the memory manager doesn't get squashed in the middle of storing
-			# an object or something.
-
-			### Restart the memory manager
+			### Restart the memory manager, clearing the current active objects
+			### and returning them.
 			def restart( visitor )
-				@managerThread.raise Reload
-				@managerThread.join
+				objs = nil
+				@mutex.synchronize( Sync::EX ) {
+					objs = self.shutdown
+					self.start( visitor )
+				}
 
-				self.start( visitor )
+				return objs
 			end
 			
 
-			### Kills the memory manager after storing all active objects
+			### Kill the memory manager and return all active, non-shallow
+			### objects.
 			def shutdown
 				@managerThread.raise Shutdown
 				@managerThread.join
+				return @activeObjects.values.reject {|o| o.shallow?}
 			end
 
 
-			### Registers the specified <tt>objects</tt> with the memory-manager
-			def register ( *objects )
+			### Registers the specified <tt>objects</tt> with the
+			### memory-manager.
+			def register( *objects )
 				checkEachType( objects, MUES::StorableObject )
 
 				@mutex.synchronize( Sync::EX ) {
 					objects.each {|o|
-						@activeObjects[o.objectStoreID] = o
+						@activeObjects[o.objectStoreId] = o
 					}
+				}
+
+				@activeObjects.rehash
+			end
+
+
+			### Clear the objectspace
+			def clear
+				self.log.notice( "Clearing active objectspace." )
+				@mutex.synchronize( Sync::EX ) {
+					@activeObjects.clear
 				}
 			end
 
