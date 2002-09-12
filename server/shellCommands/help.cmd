07fb772ef90ae669f0351c371c9c47194427fcb5
Michael Granger <ged@FaerieMUD.org>
2002-09-12 12:54:42 +0000

    Initial commit.
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40672

---
+++ server/shellCommands/help.cmd
@@ -0,0 +1,59 @@
+#
+# The help MUES::CommandShell command.
+# $Id: help.cmd,v 1.1 2002/09/12 12:54:42 deveiant Exp $
+#
+# == Authors:
+# * Michael Granger <ged@FaerieMUD.org>
+#
+
+### Help command
+= help
+
+== Abstract
+Fetch help about a command or all commands.
+
+== Description
+If a command is specified, list detailed help on the given command.
+
+If no command is given, list all available commands in a table with a brief
+description and its synonyms.
+
+== Usage
+  help [<command>]
+
+== Code
+
+  helpTable = nil
+  rows = []
+
+  ### Fetch the help table from the shell's command table
+  if argString =~ %r{(\w+)}
+	helpHash = context.shell.commandTable.getHelpForCommand( $1 )
+
+	# If there was no help available, just output a message to
+	# that effect
+	return OutputEvent.new( "No help found for '#{$1}'\n" ) if helpHash.nil?
+
+	rows << "\n#{helpHash[:name]}\n#{'-' * helpHash[:name].length}\n\n" <<
+		(helpHash[:description] || "#{helphash[:abstract]}\n\n") <<
+		"Usage:\n"
+	rows.push( *helpHash[:usage] )
+	if context.user.isImplementor?
+		rows << "Source: #{helpHash[:sourceFile]} line #{helpHash[:sourceLine]}\n"
+	end
+  else
+	helpTable = context.shell.commandTable.getHelpTable()
+	rows << "Help topics:\n"
+
+	### Add a row or two for each table entry
+	helpTable.sort.each {|cmd,desc|
+		rows << "\t%20s : %s" % [ cmd, desc[0] ]
+		rows << " [Synonyms: %s]" % desc[1].join(', ') unless desc[1].empty?
+		rows << "\n"
+	}
+  end
+
+  rows << "\n"
+  return [ OutputEvent.new( rows ) ]
+
+
