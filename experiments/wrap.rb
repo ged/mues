9d88f1648fa3c2c7d366867c4613786572645414
Michael Granger <ged@FaerieMUD.org>
2002-09-15 07:36:20 +0000

    - Rewritten to use strscan and generate correctly-wrapped output.
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40686

--- experiments/wrap.rb
+++ experiments/wrap.rb
@@ -1,26 +1,56 @@
 #!/usr/bin/ruby
 
-### Wrap the specified +text+ to the specified +width+.
-def wrap( text, width=80 )
-	workingText = text.dup
-	newText = ''
-	
-	while workingText.length > width
-		i = width
-		until workingText[i,1] =~ /\s/ do
-			raise RuntimeError, "Text is not wrappable to the specified width." if i < 1
-			i -= 1
-		end 
-		newText << workingText[ 0, i ] << "\n"
-		workingText[ 0, i + 1 ] = ''
+if $0 =~ /^experiments/
+	require 'utils.rb'
+else
+	require '../utils.rb'
+end
+
+include UtilityFunctions
+
+require 'strscan'
+
+class WrappedLineArray
+	def initialize( width=80 )
+		@width = width
+		@lines = ['']
 	end
 
-	newText << workingText
-	return newText
+	attr_accessor :lines
+
+	def append( chars )
+		if chars.length + @lines.last.length > @width
+			@lines.push( '' )
+			chars.strip!
+		end
+
+		@lines[-1] += chars
+	end
+
+end
+
+
+### Wrap the specified +text+ to the specified +width+.
+def wrap( text, width=80 )
+	lines = text.split( /\n/ )
+	wrappedLines = []
+	
+	lines.each {|line|
+		debugMsg( "Scanning line '#{line}'" )
+		wrappedLines.push WrappedLineArray::new( width )
+		scanner = StringScanner::new( line, true )
+
+		while scanner.rest?
+			appendText = scanner.scan( /\A\s*\S+/ ) || scanner.getch
+			wrappedLines.last.append( appendText )
+		end
+	}
+			
+	return wrappedLines.collect {|wla| wla.lines.join("\n")}.join("\n")
 end
 
 testString = <<-'EOF'
-This is a really long sentence that I expect to be wrapped. Ill append some other stuff just to test it out.
+This is a really long sentence that I expect to be wrapped. I'll append some other stuff just to test it out.
 
 So here, after much delay, and banging my head against many sharp things, and reading some passages I had bookmarked in various books, is my current state of thought about what Story is to FaerieMUD. These are painted with a broad brush, are still very much a work in progress, of course.
 
@@ -28,8 +58,9 @@ Apologies to those coming in in the middle of this dicussion -- feel free to ask
 EOF
 
 [ 80, 40, 20, 120 ].each {|width|
-	(120/5).times {|i| print "---%02d" % ((i+1) * 5) }
-	puts "\n" + wrap( testString, width )
+	header "Wrapping to #{width} characters."
+	(120/5).times {|i| print "|--%02d" % ((i+1) * 5) }
+	puts "\n" + wrap( testString, width ) + "\n\n"
 }
 
 
