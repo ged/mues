bdd190c83da967e7af012ddfb9b248b9b84b28a5
Michael Granger <ged@FaerieMUD.org>
2002-10-04 05:20:02 +0000

    - Updated the manifest.
    - Added a 'snapshot' mode that doesn't try to tag the source before creating an
      archive.
    
    - Added command-line option processing.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40728

--- makedist.rb
+++ makedist.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/ruby
 #
 #	MUES Distribution Maker Script
-#	$Id: makedist.rb,v 1.8 2002/05/16 03:59:50 deveiant Exp $
+#	$Id: makedist.rb,v 1.9 2002/10/04 05:20:02 deveiant Exp $
 #
-#	Copyright (c) 2001, The FaerieMUD Consortium.
+#	Copyright (c) 2001, 2002, The FaerieMUD Consortium.
 #
 #	This is free software. You may use, modify, and/or redistribute this
 #	software under the terms of the Perl Artistic License. (See
 #	http://language.perl.com/misc/Artistic.html)
 #
+
+require 'getoptlong'
+require 'ftools'
+require "./utils.rb"
+
+include UtilityFunctions
+
 
 ### Configuration stuff
 
@@ -16,22 +23,23 @@
 MANIFEST = %w{
   Artistic
   ChangeLog
+  CONFIGURATION
   INSTALL
   QUICKSTART
   README
   docs/lib/**/*
   docs/makedocs.rb
   docs/stylesheets/*
+  ext/extconf.rb
+  ext/mues/*
   install.rb
   lib/**/*
   quickstart.rb
-  server/MUES.cfg.example
-  server/bin/createUser.rb
-  server/bin/deleteUser.rb
   server/bin/mues.rb
-  server/bin/showUser.rb
   server/environments/*
-  server/shellCommands/**/*
+  server/minimal-config.xml
+  server/shellCommands/*
+  test.rb
   tests/**/*
   utils.rb
 }
@@ -48,20 +56,17 @@ ANTIMANIFEST = [
 	/\.cvsignore/
 ]
 
+Options = [
+	[ "--snapshot",	"-s",		GetoptLong::NO_ARGUMENT ],
+	[ "--verbose",  "-v",		GetoptLong::NO_ARGUMENT ],
+]
+
 ### End of configuration
 
-require 'ftools'
-require "./utils.rb"
-
-include UtilityFunctions
 
 # Version information
-Version = /([\d\.]+)/.match( %q$Revision: 1.8 $ )[1]
-Rcsid = %q$Id: makedist.rb,v 1.8 2002/05/16 03:59:50 deveiant Exp $
-
-# Set interrupt handler to restore tty before exiting
-stty_save = `stty -g`.chomp
-trap("INT") { system "stty", stty_save; exit }
+Version = /([\d\.]+)/.match( %q$Revision: 1.9 $ )[1]
+Rcsid = %q$Id: makedist.rb,v 1.9 2002/10/04 05:20:02 deveiant Exp $
 
 $Programs = {
 	'tar'	=> nil,
@@ -112,21 +117,43 @@ Distros = [
 ]
 
 
+# Set interrupt handler to restore tty before exiting
+stty_save = `stty -g`.chomp
+trap("INT") { system "stty", stty_save; exit }
+
 ### Main function
 def main
 	filelist = []
+	snapshot = false
+	verbose = false
+
+	# Read command-line options
+	opts = GetoptLong::new( *Options )
+	opts.each do |opt, arg|
+		case opt
+
+		when '--snapshot'
+			snapshot = true
+
+		when '--verbose'
+			verbose = true
+
+		else
+			MUES::Log.error( "No such option '#{opt}'" )
+		end
+			
+	end
 
 	project = File.open( "CVS/Repository", "r").readline.chomp
 	header "%s Distribution Maker" % project
 
-	releaseVersion = extractNextVersionFromTags( MANIFEST[0] )
-
-	message "Finding necessary programs...\n"
+	message "Finding necessary programs...\n\n"
 	for prog in $Programs.keys
-		message "  #{prog}: "
-		$Programs[ prog ] = findProgram( prog )
-		message( ($Programs[prog] || '(not found)') + "\n" )
+		$Programs[ prog ] = findProgram( prog ) or
+			abort "Required program #{prog} not found."
+		replaceMessage( "  #{prog}: %s\n" % $Programs[prog] )
 	end
+	replaceMessage( "All required programs found.\n" )
 
 	message "Building manifest..."
 	for pat in MANIFEST
@@ -137,22 +164,32 @@ def main
 
 	message "Vetting manifest..."
 	for regex in ANTIMANIFEST
-		$stderr.puts "Pattern /#{regex.source}/ removed: " + filelist.find_all {|file| regex.match(file)}.join(', ')
+		if verbose
+			$stderr.puts "Pattern /#{regex.source}/ removed: " +
+				filelist.find_all {|file| regex.match(file)}.join(', ')
+		end
 		filelist.delete_if {|file| regex.match(file)}
 	end
-	message "removed #{origLength - filelist.length} files.\n"
+	message "removed #{origLength - filelist.length} files from the list.\n"
 
 	#puts "Filelist:\n\t" + filelist.join("\n\t")
 
-	version = promptWithDefault( "Distribution version", releaseVersion )
-	distName = "%s-%s" % [ project, version ]
+	version = distName = nil
+	if snapshot
+		version = promptWithDefault( "Snapshot version", Time::now.strftime('%Y%m%d') )
+		distName = "%s-%s" % [ project, version ]
+	else
+		releaseVersion = extractNextVersionFromTags( MANIFEST[0] )
+		version = promptWithDefault( "Distribution version", releaseVersion )
+		distName = "%s-%s" % [ project, version ]
 
-	tag = "RELEASE_%s" % sprintf('%0.2f', version).gsub(/\./, '_') 
-	tagFlag = promptWithDefault( "Tag '%s' with %s" % [ project, tag ], 'y' )
+		tag = "RELEASE_%s" % sprintf('%0.2f', version).gsub(/\./, '_') 
+		tagFlag = promptWithDefault( "Tag '%s' with %s" % [ project, tag ], 'y' )
 
-	if tagFlag =~ /^y/i
-		$stderr.puts "Running #{$Programs['cvs']} -q tag #{tag}"
-		system $Programs['cvs'], '-q', 'tag', tag
+		if tagFlag =~ /^y/i
+			$stderr.puts "Running #{$Programs['cvs']} -q tag #{tag}"
+			system $Programs['cvs'], '-q', 'tag', tag
+		end
 	end
 
 	message "Making distribution directory #{distName}..."
