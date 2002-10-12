1af231cee7ff3e99275810593b9e1d71ee9d04c6
Michael Granger <ged@FaerieMUD.org>
2002-10-12 11:23:25 +0000

    - Added command-line arguments to facilitate testing more than just the   BerkeleyDB backend.
    - Removed the now-superfluous timeout code.
    
    - Improved comment documentation.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40770

--- experiments/userobjstore.rb
+++ experiments/userobjstore.rb
@@ -13,44 +13,88 @@ require 'mues/ObjectStore'
 require 'mues/User'
 
 require 'timeout'
+require 'getoptlong'
 #require 'profile'
 
-$backend = 'BerkeleyDB'
+$backend = 'Flatfile'
 $memmgr = 'Null'
+
+# Read command-line options
+opts = GetoptLong.new
+opts.set_options(
+	[ '--backend',	'-b',	GetoptLong::REQUIRED_ARGUMENT ],
+	[ '--memmgr',	'-m',	GetoptLong::REQUIRED_ARGUMENT ],
+	[ '--debug',	'-d',	GetoptLong::NO_ARGUMENT ],
+	[ '--help',		'-h',	GetoptLong::NO_ARGUMENT ]
+)
+
+opts.each {|opt, val|
+	case opt
+	when '--backend'
+		$backend = val
+
+	when '--memmgr'
+		$memmgr = val
+
+	when '--debug'
+		$DEBUG = true
+
+	end
+}
+
+
+#####################################################################
+###	B E G I N   T E S T S
+#####################################################################
 
 header "Testing store/fetch of a user in a #$backend objectstore"
 
+# Create objectstore
 header "Creating objectstore"
 os = MUES::ObjectStore::create( :name => "testuser",
 							    :backend => $backend,
 							    :memmgr => $memmgr,
 							    :indexes => %w{class username}
 							   )
+os.debugLevel = 5 if $DEBUG
 
+# Create a new user
 header "Creating user"
 user = MUES::User::new( :username => 'ged',
 					    :realname => 'Ged',
-					    :emailAddress => 'ged@FaerieMUD.org' )
+					    :emailAddress => 'ged@FaerieMUD.org',
+					    :password => 'furchtbar' )
+user.debugLevel = 5 if $DEBUG
+
 userId = user.muesid
 message ("-" * 60) + "\nUser => #{user.inspect}\n" + ("-" * 60) + "\n"
 							    
+# Store the user
 header "Storing user"
-#timeout( 3 ) {
-	os.store( user )
-#}
+os.store( user )
 
+# Unset the user variable
 header "Freeing user"
 user = nil
 
+# Fetch the user by id
+header "Fetching user by id"
+user = os.retrieve( userId )
+
+writeLine
+message "User => #{user.inspect}"
+writeLine
+
+# Look up the user using indexes
 header "Looking user up by class+name:"
-#timeout( 3 ) {
-	matches = os.lookup( :class => MUES::User, :username => 'ged' )
-	message ("-" * 60) + "\nMatches => #{matches.inspect}\n" + ("-" * 60) + "\n"
-#}
+matches = os.lookup( :class => MUES::User, :username => 'ged' )
+message ("-" * 60) + "\nMatches => #{matches.inspect}\n" + ("-" * 60) + "\n"
 
+# Unset the user variable again
 header "Freeing user"
 user = nil
 
+# Try looking the user up repeatedly by id
 10.times do |count|
 	user = nil
 	header "Fetching user by id (iteration #{count})"
@@ -61,15 +105,13 @@ user = nil
 	os.store( user )
 end
 
+# Close the objectstore
 header "Closing objectstore"
-#timeout( 3 ) {
-	os.close
-#}
+os.close
 
+# Drop the objectstore
 header "Dropping objectstore"
-#timeout( 3 ) {
-	os.drop
-#}
+os.drop
 
 
 
