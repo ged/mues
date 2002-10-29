f5d7e05ab7cd545cda8870d9c500e646f2ba66da
Michael Granger <ged@FaerieMUD.org>
2002-10-29 07:37:41 +0000

    - Added new command: 'context'
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%40967

--- server/shellCommands/eval.cmd
+++ server/shellCommands/eval.cmd
@@ -1,8 +1,8 @@
 # -*- default-generic -*-
 #
 # The eval MUES::CommandShell command.
-# Time-stamp: <22-Oct-2002 22:01:00 deveiant>
-# $Id: eval.cmd,v 1.1 2002/10/23 05:00:28 deveiant Exp $
+# Time-stamp: <27-Oct-2002 17:33:11 deveiant>
+# $Id: eval.cmd,v 1.2 2002/10/29 07:37:41 deveiant Exp $
 #
 # == Authors:
 # * Michael Granger <ged@FaerieMUD.org>
@@ -29,6 +29,49 @@ context object and outputs the result.
 == Code
   return [MUES::EvalCommandEvent::new( argString, context.evalContext, context.user )]
 
+
+
+### 'Context' command
+= context
+
+== Restriction
+admin
+
+== Abstract
+Set the object target of the /eval command to the object specified by the given id.
+
+== Description
+
+This command either displays the inspected form of the shell's context eval
+target, if no id is specified, or looks up an object by Ruby id and sets the
+shell context's target object to the resultant object if an id is given.
+
+== Usage
+  context [<id>]
+
+== Code
+  
+  	msg = ''
+
+	# If there weren't any arguments, just show the current context object
+	if argString.empty?
+		msg = "Current context object is:\n\n%s\n\n" %
+			MUES::UtilityFunctions::trimString( context.evalContext.inspect, 60 )
+
+	elsif argString =~ /^(\d+)$/
+		id = $1.to_i
+
+		obj = MUES::UtilityFunctions::getObjectByRubyId( id )
+		context.evalContext = obj
+
+		msg = "Set context object to:\n\n%s\n\n" % 
+			MUES::UtilityFunctions::trimString( obj.inspect, 60 )
+
+	else
+		raise MUES::CommandError, self.usage
+	end
+
+	return [MUES::OutputEvent::new(msg)]
 
 
 
