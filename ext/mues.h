f9276561f3eb39ffbf0cd37349a6f3f9be09cfe2
Michael Granger <ged@FaerieMUD.org>
2003-04-19 06:53:33 +0000

    - Made debugging changes to facilitate building extensions without debugging   code.
    - Added definitions for MUES::BlankObject.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%401018

--- ext/mues.h
+++ ext/mues.h
@@ -1,12 +1,12 @@
 /*
  *	mues.h - header file for MUES C extensions
- *	$Id: mues.h,v 1.1 2002/06/04 06:44:21 deveiant Exp $
+ *	$Id: mues.h,v 1.2 2003/04/19 06:53:33 deveiant Exp $
  *
  *	Authors:
  *		Martin Chase <stillflame@FaerieMUD.org>
  *		Michael Granger <ged@FaerieMUD.org>
  *
- *	Copyright (c) 2002 The FaerieMUD Consortium. All rights reserved.
+ *	Copyright (c) 2002, 2003 The FaerieMUD Consortium. All rights reserved.
  *
  *  This module is free software. You may use, modify, and/or redistribute this
  *  software under the terms of the Perl Artistic License. (See
@@ -31,16 +31,29 @@
 /* System includes */
 #include <stdio.h>
 
-// Debugging function
+/* Debugging macro to facilitate turning debugging messages off in production
+   builds. */
+#ifdef DEBUG
+#define DebugMsg(a) mues_debug a
+#else  /* !DEBUG */
+#define DebugMsg(a)
+#endif /* DEBUG */
+
+/* Debugging message function */
 #ifdef HAVE_STDARG_PROTOTYPES
+
 #include <stdarg.h>
 #define va_init_list(a,b) va_start(a,b)
 void mues_debug(const char *fmt, ...);
-#else
+
+#else  /* !HAVE_STDARG_PROTOTYPES */
+
 #include <varargs.h>
 #define va_init_list(a,b) va_start(a)
 void mues_debug(fmt, va_alist);
-#endif
+
+#endif /* HAVE_STDARG_PROTOTYPES */
+
 
 /*
  * Globals
@@ -52,6 +65,7 @@ extern VALUE mues_mMUES;
 // Classes
 extern VALUE mues_cMuesObject;				// MUES::Object
 extern VALUE mues_cMuesPolymorphicObject;	// MUES::PolymorphicObject
+extern VALUE mues_cMuesBlankObject;			// MUES::BlankObject
 
 // Exception classes
 extern VALUE mues_eVirtualMethodError;
@@ -61,6 +75,7 @@ extern VALUE mues_eVirtualMethodError;
 extern void Init_mues						_(());
 extern void Init_Mues_PolymorphicObject		_(());
 extern void Init_Mues_Object				_(());
+extern void Init_Mues_BlankObject			_(());
 
 
 #endif /* _MUES_H */
