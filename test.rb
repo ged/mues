54d855069972882ef7bd22014cfccf03b7273ce3
Michael Granger <ged@FaerieMUD.org>
2003-06-06 22:23:46 +0000

    - Cleaned up in tandem with the FaerieMUD test runner.
    - Added command-line options.
    
    - Wrapped nasty warning-generating code in a non-verbose block.
    
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%401045

--- test.rb
+++ test.rb
@@ -7,45 +7,73 @@
 BEGIN {
 	$:.unshift "lib", "ext", "tests"
 
-	require "mues/Log"
-	require "log4r"
-	require "log4r/outputter/fileoutputter"
-	MUES::Log::mueslogger.outputters =
-		Log4r::FileOutputter::new( 'logfile',
-								  :filename => 'test.log',
-								  :trunc => true )
+	require './utils'
+	include UtilityFunctions
 
-	# Workaround for the sprintf error that shows up in the simpleformatter in
-	# later versions of Ruby 1.7.3.
-	Log4r::Outputter['logfile'].formatter =
+	verboseOff {
+		require "mues/Log"
+		require "log4r"
+		require "log4r/outputter/fileoutputter"
+		MUES::Log::mueslogger.outputters =
+		Log4r::FileOutputter::new( 'logfile',
+			:filename => 'test.log',
+			:trunc => true )
+
+		# Workaround for the sprintf error that shows up in the simpleformatter in
+		# later versions of Ruby 1.7.3.
+		Log4r::Outputter['logfile'].formatter =
 		Log4r::PatternFormatter::new( :pattern => '[%d] [%l] %C: %.1024m',
-									  :date_pattern => '%Y/%m/%d %H:%M:%S %Z' )
-	
+			:date_pattern => '%Y/%m/%d %H:%M:%S %Z' )
+	}
 }
 
-require './utils'
-include UtilityFunctions
+verboseOff {
+	require 'tests/muesunittest'
+	require 'find'
+	require 'test/unit'
+	require 'test/unit/testsuite'
+	require 'test/unit/ui/console/testrunner'
+	require 'optparse'
+}
 
-require 'find'
-require 'test/unit'
-require 'test/unit/testsuite'
-require 'test/unit/ui/console/testrunner'
-
+# Initialize variables
+safelevel = 0
 patterns = []
-ARGV.each {|pat| patterns << Regexp::new( pat )}
-
 requires = []
+
+# Parse command-line switches
+ARGV.options {|oparser|
+	oparser.banner = "Usage: #$0 [options] [TARGETS]\n"
+
+	oparser.on( "--debug", "-d", TrueClass, "Turn debugging on" ) {
+		$DEBUG = true
+		debugMsg "Turned debugging on."
+	}
+
+	# Handle the 'help' option
+	oparser.on( "--help", "-h", "Display this text." ) {
+		$stderr.puts oparser
+		exit!(0)
+	}
+
+	oparser.parse!
+}
+
+# Parse test patterns
+ARGV.each {|pat| patterns << Regexp::new( pat, Regexp::IGNORECASE )}
+$stderr.puts "#{patterns.length} patterns given on the command line"
 
 ### Load all the tests from the tests dir
 Find.find("tests") {|file|
-	Find.prune if file =~ /^.'/ or file =~ /~$/
-	Find.prune if file =~ /TEMPLATE/
+	Find.prune if /\/\./ =~ file or /~$/ =~ file
+	Find.prune if /TEMPLATE/ =~ file
 	next if File.stat( file ).directory?
 
  	unless patterns.empty?
  		Find.prune unless patterns.find {|pat| pat =~ file}
  	end
 
+	debugMsg "Considering '%s': " % file
 	next unless file =~ /\.tests.rb$/
 	debugMsg "Requiring '%s'..." % file
 	require "#{file}"
@@ -57,20 +85,30 @@ unless patterns.empty?
 	$stderr.puts "[" + requires.sort.join( ", " ) + "]"
 end
 
+# Build the test suite
 class MUESTests
 	class << self
 		def suite
-			suite = Test::Unit::TestSuite.new( "Multi-User Environment Server" )
+			suite = Test::Unit::TestSuite.new( "MUES" )
 
-			ObjectSpace.each_object( Class ) {|klass|
-				suite.add( klass.suite ) if klass < Test::Unit::TestCase
-			}			
+			if suite.respond_to?( :add )
+				ObjectSpace.each_object( Class ) {|klass|
+					suite.add( klass.suite ) if klass < MUES::TestCase
+				}
+			else
+				ObjectSpace.each_object( Class ) {|klass|
+					suite << klass.suite if klass < MUES::TestCase
+				}			
+			end
 
 			return suite
 		end
 	end
 end
 
+# Run tests
+$SAFE = safelevel
+MUES::Log.mueslogger.level = 0 if $DEBUG
 Test::Unit::UI::Console::TestRunner.new( MUESTests ).start
 
 
