8456f3d8322758f2f269542f9e0cb90b8269248e
Michael Granger <ged@FaerieMUD.org>
2003-10-13 04:07:11 +0000

    Initial commit.
    --HG--
    extra : convert_revision : svn%3A39a105f4-52d6-0310-bb1b-d6517044f2e4/trunk%401101

---
+++ experiments/changeRequireCases.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/ruby
+# 
+# Script to rewrite requires in MUES files to reflect change in file-naming
+# convention to all-lowercase.
+# 
+# == Rcsid
+# 
+# $Id: changeRequireCases.rb,v 1.1 2003/10/13 04:07:11 deveiant Exp $
+# 
+# == Authors
+# 
+# * Michael Granger <ged@FaerieMUD.org>
+# 
+# Copyright (c) 2001-2003, The FaerieMUD Consortium.
+#
+# This is free software. You may use, modify, and/or redistribute this software
+# under the terms of the Perl Artistic License. (See
+# http://language.perl.com/misc/Artistic.html)
+#
+
+require "./utils.rb"
+include UtilityFunctions
+
+header "MUES require-rewrite script"
+
+%w{lib server experiments tests}.each {|dir|
+	message "Searching #{dir}...\n"
+
+	Dir["#{dir}/**/*"].each do |file|
+		next if !File::file?( file ) ||
+			/CVS/ =~ file
+		message "Editing #{file}...\n"
+
+		editInPlace( file ) {|line|
+			line.gsub( /require (["'])mues\/([^'"]+)\1/ ) {
+				%q{require 'mues/%s'} % Regexp::last_match.captures[1].downcase
+			}
+		}
+
+		divider( 75 )
+	end
+}
+
+
