9d0a0c62dce75844451f1d07572fe4fa646857ee
Michael Granger <ged@FaerieMUD.org>
2009-07-27 22:44:11 -0700

    Updated the build system, added some design docs

---
+++ experiments/qnostart.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+require 'pp'
+require 'rubygems'
+require 'bunny'
+
+# An experiment to see if you can avoid calling 'start' on a new Bunny.
+
+amqp = Bunny.new
+amqp.start # Bombs if you don't call this explicitly. ugh.
+
+q = amqp.queue( 'chunker' )
+ex = amqp.exchange( 'log', :type => :topic )
+
+q.bind( ex, :key => 'apache.*' )
+
+pp q.pop
+
